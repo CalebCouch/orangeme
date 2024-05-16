0c3b9e65c3dc716d8ef39c516b9030f1812ee0eb
dav-anderson <david@orange.me>
2024-05-16 09:57:08 -0400

    added color constants

---
+++ lib/styles/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Greys
+  static const Color black = Colors.black;
+  static const Color offBlack = Color(0xFF262626);
+  static const Color darkGrey = Color(0xFF515357);
+  static const Color grey = Color(0xFF8A8C93);
+  static const Color lightGrey = Color(0xFFD7D8E5);
+  static const Color offWhite = Color(0xFFF4F5F5);
+  static const Color white = Colors.white;
+
+  //Colors
+  static const Color red = Color(0xFFFF3B00);
+  static const Color green = Color(0xFF00CC00);
+  static const Color orange = Color(0xFFFF6623);
+
+  //Layout
+  static const Color background = Colors.black;
+  static const Color backgroundSecondary = Color(0xFF262626);
+  static const Color border = Color(0xFF262626);
+
+  //Typography
+  static const Color heading = Colors.white;
+  static const Color text = Color(0xFFD7D8E5);
+  static const Color textSecondary = Color(0xFF8A8C93);
+
+  //Interactive
+  static const Color primary = Colors.white;
+  static const Color handle = Colors.black;
+  static const Color colorHandle = Colors.white;
+  static const Color outline = Color(0xFF515357);
+
+  //Special
+  static const Color brand = Colors.white;
+  static const Color danger = Color(0xFFFF3800);
+  static const Color success = Color(0xFF00CC00);
+  static const Color bitcoin = Color(0xFFFF6623);
+}
