e3fbed4d844df4d8a1989b8413985349f12821a3
dav-anderson <david@orange.me>
2024-05-22 13:34:37 -0400

    added animations and haptic feedback to invalid keyboard prompts on send1

--- lib/screens/non_premium/send1.dart
+++ lib/screens/non_premium/send1.dart
@@ -15,152 +15,13 @@ class Send1 extends StatefulWidget {
 }
 
 class Send1State extends State<Send1> {
-  String amount = "0.00";
+  String amount = "0";
+  final GlobalKey<KeyboardValueDisplayState> _displayKey =
+      GlobalKey<KeyboardValueDisplayState>();
   bool isButtonEnabled = false;
 
-  // void _updateAmount(String input) {
-  //   //determine the max amount the user can input in dollar terms
-  //   // double maxDollarAmount = (widget.balance / 100000000) * widget.price!;
-  //   double maxDollarAmount = 9999999;
-  //   print("Max Dollar Amount to spend: $maxDollarAmount");
-  //   //define a temporary value for parsing user input
-  //   String tempAmount = '0.00';
-  //   //if the user inputs backspace, exclude the input from temp value parse
-  //   if (input.contains("backspace")) {
-  //     print("backspace detected, override input in parse");
-  //     tempAmount = amount;
-  //     //set the temp value to old amount + new input
-  //   } else {
-  //     print("no backspace detected, proceed with standard parse");
-  //     tempAmount = amount + input;
-  //   }
-  //   //if the temp amount is an invalid double, override
-  //   if (tempAmount == "0." ||
-  //       tempAmount == "0.00." ||
-  //       tempAmount == "." ||
-  //       tempAmount == '') {
-  //     print("tempAmount invalid double, override to 0");
-  //     tempAmount = "0.00";
-  //     //if the temp amount contains the backspace string, override
-  //   } else if (tempAmount.contains("backspace")) {
-  //     print("backspace found in tempAmount, override to 0");
-  //     tempAmount = "0.00";
-  //   }
-  //   //establish a substring list to limit decimal places to 0.00
-  //   List<String> parts = [];
-  //   print("Temp Amount: $tempAmount");
-  //   double? inputNull = double.tryParse(input);
-  //   //split the sanitized temp value by the decimal point if it exists
-  //   if (tempAmount.contains(".")) {
-  //     parts = tempAmount.split('.');
-  //     print("parts: $parts");
-  //     //user attempts to enter more than one trailing zero after the decimal, force stop input and terminate
-  //     if (tempAmount == "0.00" && input == "0") {
-  //       setState(() {
-  //         amount = amount;
-  //       });
-  //       evaluateButton(input);
-  //       return;
-  //       //user attempts an input from 0.00 state that would exceed max balance, force stop input and terminate
-  //     } else if (inputNull != null &&
-  //         tempAmount == "0.00" &&
-  //         input != '.' &&
-  //         double.parse(input) > maxDollarAmount) {
-  //       print("**********input would exceed max balance*********");
-  //       evaluateButton(input);
-  //       return;
-  //       //user is attempting to exceed the decimal threshold but can be assumed to be entering a leading whole number from 0.00, proceed with input & terminate
-  //     } else if (tempAmount == "0.00" && input != "backspace" && input != ".") {
-  //       setState(() {
-  //         print("tempAmount 0.00 detected, proceeding with input");
-  //         amount = "$input";
-  //       });
-  //       evaluateButton(input);
-  //       return;
-  //       //user attempts to exceed 2 decimal threshold, force stop input & terminate
-  //     } else if (parts.length == 2 &&
-  //         parts[1].length > 2 &&
-  //         input != "backspace") {
-  //       print("decimal constraint exceeded");
-  //       evaluateButton(input);
-  //       return;
-  //     }
-  //   }
-  //   //standard keyboard inputs
-  //   double? tempAmountNull = double.tryParse(tempAmount);
-  //   setState(() {
-  //     //user deletes the most right character
-  //     if (input == 'backspace') {
-  //       //standard backspace input
-  //       if (amount.length > 1) {
-  //         print("backspace triggered");
-  //         amount = amount.substring(0, amount.length - 1);
-  //         //user attempts to delete an empty field, force stop input
-  //       } else {
-  //         print("backspace triggered, empty");
-  //         amount = "";
-  //       }
-  //       //user attempts to exceed max Balance, force stop input
-  //     } else if (tempAmountNull != null &&
-  //         double.parse(tempAmount) >= maxDollarAmount) {
-  //       print("*********max balance exceeded***********");
-  //       //user inputs values <$1
-  //     } else if (amount == "0.00") {
-  //       //user inputs a decimal, assume they want <$1
-  //       if (input == '.') {
-  //         print("inputting less than a dollar");
-  //         amount = "0.";
-  //         //user inputs a whole number with no decimal prefix, clear the field and accept the input as a leading whole number
-  //       } else {
-  //         print("assuming whole number");
-  //         amount = input;
-  //       }
-  //       //user enters an integer with a leading zero and no decimal, assume whole number and force input
-  //     } else if (amount == "0" && input != ".") {
-  //       print("leading zero, no decimal place, assume whole number");
-  //       amount = input;
-  //       //user attempts to enter more than one leading zero before a decimal, assume user wants <$1 and force input
-  //     } else if (input == "0" && (amount == "0" || amount == "0.00")) {
-  //       print("multiple leading zeros, assume decimal");
-  //       amount = "0.";
-  //       //user provides a standard input not covered above, behaves as expected
-  //     } else if (input == '.' &&
-  //         (amount == '' ||
-  //             amount == "0" ||
-  //             amount == "0.0" ||
-  //             amount == "0.00")) {
-  //       amount = "0.";
-  //     } else {
-  //       print("standard input");
-  //       amount += input;
-  //     }
-  //     evaluateButton(input);
-  //   });
-  // }
-
-  // void evaluateButton(String input) {
-  //   setState(() {
-  //     //user has only 1 character and they input a backspace, disable button
-  //     double? amountNull = double.tryParse(amount);
-  //     if (input.contains("backspace") && amount.length <= 1) {
-  //       print(
-  //           "amount less than 1 character, user deletion input detected, button disabled");
-  //       isButtonEnabled = false;
-  //       //users input results in a value exceeding one penny, enable button (set dust limit here?)
-  //       //threshold is set to 1/100 of a penny due to temp value (amount + input concatenation)'s effect on baseline 0.00, inputs < 0.01 are rejected
-  //     } else if (amountNull != null && double.parse(amount) >= 0.001) {
-  //       print("amount check exceeds one penny, button enabled");
-  //       isButtonEnabled = true;
-  //       //users input does not exceed one penny, disable button
-  //     } else {
-  //       print("amount check failed, button disabled");
-  //       isButtonEnabled = false;
-  //     }
-  //   });
-  // }
-
   void _updateAmount(String input) {
-    double maxDollarAmount = 9999999; // Set the max dollar amount
+    double maxDollarAmount = (widget.balance / 100000000) * widget.price!;
     String tempAmount = amount; // Current displayed amount
 
     if (input == "backspace") {
@@ -190,13 +51,14 @@ class Send1State extends State<Send1> {
     }
     // Ensure tempAmount is a valid dollar amount with at most two decimal places
     if (!RegExp(r"^\d*\.?\d{0,2}$").hasMatch(tempAmount)) {
+      _displayKey.currentState?.shake();
       return; // Return if not a valid format
     }
     // Convert tempAmount to a double and check it doesn't exceed max amount
     double? tempAmountDouble = double.tryParse(tempAmount);
     if (tempAmountDouble != null && tempAmountDouble > maxDollarAmount) {
+      _displayKey.currentState?.shake();
       return; // Prevent exceeding the maximum amount
-      //TODO add max balance feedback here
     }
     setState(() {
       amount = tempAmount;
@@ -250,10 +112,12 @@ class Send1State extends State<Send1> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             KeyboardValueDisplay(
+              key: _displayKey,
               fiatAmount: amount == '' ? '0' : amount,
               quantity: amount == ''
                   ? formatDollarsToSats('0', widget.price)
                   : formatDollarsToSats(amount, widget.price),
+              onShake: () {},
             ),
             const SizedBox(height: 10),
             NumberPad(
--- lib/widgets/keyboard_value_display.dart
+++ lib/widgets/keyboard_value_display.dart
@@ -1,83 +1,133 @@
 import 'package:flutter/material.dart';
-import 'package:orange/styles/constants.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:orange/styles/constants.dart';
 
-class KeyboardValueDisplay extends StatelessWidget {
+class KeyboardValueDisplay extends StatefulWidget {
   final String fiatAmount;
   final String quantity;
+  final VoidCallback onShake;
 
   const KeyboardValueDisplay({
     super.key,
     required this.fiatAmount,
     required this.quantity,
+    required this.onShake,
   });
 
+  @override
+  KeyboardValueDisplayState createState() => KeyboardValueDisplayState();
+}
+
+class KeyboardValueDisplayState extends State<KeyboardValueDisplay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _shakeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _shakeAnimation = Tween<double>(begin: 0, end: 10).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.elasticIn),
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _animationController.reverse();
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void shake() {
+    _animationController.forward(from: 0);
+    HapticFeedback.mediumImpact();
+  }
+
   String formatFiatAmount(String fiatAmount) {
+    if (fiatAmount.endsWith(".") ||
+        fiatAmount.endsWith(".0") ||
+        fiatAmount.endsWith(".00")) {
+      // If the amount ends with a decimal point or zeroes, return as is.
+      return fiatAmount;
+    }
     double? number = double.tryParse(fiatAmount);
     if (number == null) {
-      return "0.00";
+      return "0"; // Default to 0 if parsing fails
     } else {
-      if (fiatAmount.length > 4) {
-        NumberFormat format = NumberFormat("#,##0.00", "en_US");
-        String formatted = format.format(number);
-        return formatted;
-      } else {
-        return fiatAmount;
-      }
+      NumberFormat format = NumberFormat("#,###.##", "en_US");
+      return format.format(number);
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    print("Dashboard Value Builder...");
-    print("fiat Amount: $fiatAmount");
-    print("quantity: $quantity");
-    double? fiatAmountNull = double.tryParse(fiatAmount);
-    bool bigNumber = false;
-    if (fiatAmountNull != null &&
-        double.parse(fiatAmount).toString().length >= 7) {
-      print("BIG NUMBER FOUND");
-      bigNumber = true;
-    }
-    return Container(
-      width: 288,
-      height: 221,
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 64),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text.rich(
-            TextSpan(
-              children: <TextSpan>[
+    print("widget fiat amount: ${widget.fiatAmount}");
+    print("Formatted fiat amount: ${formatFiatAmount(widget.fiatAmount)}");
+    return AnimatedBuilder(
+      animation: _shakeAnimation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(_shakeAnimation.value, 0),
+          child: child,
+        );
+      },
+      child: Container(
+        width: 288,
+        height: 221,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 64),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _shakeAnimation,
+              builder: (context, child) {
+                return Transform.translate(
+                  offset: Offset(_shakeAnimation.value, 0),
+                  child: child,
+                );
+              },
+              child: Text.rich(
                 TextSpan(
-                    text: "\$",
-                    style: !bigNumber
-                        ? AppTextStyles.heading1
-                        : AppTextStyles.heading2),
-                TextSpan(
-                    text: formatFiatAmount(fiatAmount),
-                    style: !bigNumber
-                        ? AppTextStyles.heading1
-                        : AppTextStyles.heading2),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: "\$",
+                        style: widget.fiatAmount.length >= 7
+                            ? AppTextStyles.heading2
+                            : AppTextStyles.heading1),
+                    TextSpan(
+                        text: formatFiatAmount(widget.fiatAmount),
+                        style: widget.fiatAmount.length >= 7
+                            ? AppTextStyles.heading2
+                            : AppTextStyles.heading1),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(widget.quantity.toString(), style: AppTextStyles.textLG),
+                const SizedBox(width: 6),
+                const Text(
+                  ' BTC',
+                  style: AppTextStyles.textLG,
+                ),
               ],
             ),
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(quantity.toString(), style: AppTextStyles.textLG),
-              const SizedBox(width: 6),
-              const Text(
-                ' BTC',
-                style: AppTextStyles.textLG,
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
