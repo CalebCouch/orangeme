d87a591f647f14880c30ad1b03381481e35825d6
dav-anderson <david@orange.me>
2024-05-24 16:14:24 -0400

    removed deprecated imports

--- lib/screens/non_premium/send5.dart
+++ lib/screens/non_premium/send5.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:orange/screens/non_premium/dashboard.dart';
 import 'package:orange/components/buttons/secondary_lg.dart';
 import 'package:orange/styles/constants.dart';
-import 'package:orange/classes.dart';
 
 class Send5 extends StatefulWidget {
   final int amount;
