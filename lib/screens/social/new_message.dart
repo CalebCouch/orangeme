fa5bc3d226d721d658acbadc43a0dee10a6749d8
dav-anderson <david@orange.me>
2024-06-04 12:39:46 -0400

    added screens for a new message

---
+++ lib/screens/social/new_message.dart
@@ -0,0 +1,41 @@
+import 'package:orange/widgets/mode_navigator.dart';
+import 'package:orange/styles/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:orange/screens/non_premium/dashboard.dart';
+import 'package:orange/components/textfield.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  NewMessageState createState() => NewMessageState();
+}
+
+class NewMessageState extends State<NewMessage> {
+  final TextEditingController recipientAddressController =
+      TextEditingController();
+
+  void navigate() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const Dashboard()));
+  }
+
+  void newMessage() {
+    print("new message button selected");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: const Text('New message'),
+      ),
+      body: const SizedBox(height: 10),
+      TextInputField(
+        controller: recipientAddressController,
+        hint: "Profile Name",
+      ),
+    );
+  }
+}
--- lib/screens/social/social.dart
+++ lib/screens/social/social.dart
@@ -2,6 +2,8 @@ import 'package:orange/widgets/mode_navigator.dart';
 import 'package:orange/styles/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:orange/screens/non_premium/dashboard.dart';
+import 'package:orange/components/buttons/orange_lg.dart';
+import 'new_message.dart';
 
 class SocialDashboard extends StatefulWidget {
   const SocialDashboard({super.key});
@@ -16,6 +18,11 @@ class SocialDashboardState extends State<SocialDashboard> {
   void navigate() {
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const Dashboard()));
+  }
+
+  void newMessage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const NewMessage()));
   }
 
   @override
@@ -33,8 +40,16 @@ class SocialDashboardState extends State<SocialDashboard> {
           children: <Widget>[
             Text(
               'No chats yet.\nGet stated by messaging a friend.',
+              textAlign: TextAlign.center,
               style:
                   AppTextStyles.textMD.copyWith(color: AppColors.textSecondary),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ButtonOrangeLG(
+                label: "New Message",
+                onTap: () => newMessage(),
+              ),
             ),
           ],
         ),
