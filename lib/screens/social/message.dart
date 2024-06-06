0f6baa851ad7c003ead95c0ba2fdb1d978fd8181
dav-anderson <david@orange.me>
2024-06-06 15:13:25 -0400

    added a message_appbar component, added some logic to selecting a contact in the new message screen, which should now take the user to a message screen styled apropriately for the selected contact

--- lib/screens/social/message.dart
+++ lib/screens/social/message.dart
@@ -1,44 +1,62 @@
-import 'package:orange/styles/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:orange/screens/non_premium/dashboard.dart';
-import 'package:orange/widgets/text_bauble.dart';
+import 'package:orange/styles/constants.dart';
+import 'package:orange/widgets/message_appbar.dart';
+import 'package:orange/components/textfield.dart';
 
 class Message extends StatefulWidget {
-  const Message({super.key});
+  final String contactName;
+  final String imagePath;
+  const Message({
+    super.key,
+    required this.contactName,
+    this.imagePath = AppImages.defaultProfileMD,
+  });
 
   @override
   MessageState createState() => MessageState();
 }
 
 class MessageState extends State<Message> {
-  final TextEditingController recipientAddressController =
-      TextEditingController();
-
-  void navigate() {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const Dashboard()));
-  }
+  final TextEditingController messageController = TextEditingController();
+  List<String> messages = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: const Text('Contact Name'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'No messaages yet.',
-              textAlign: TextAlign.center,
-              style:
-                  AppTextStyles.textMD.copyWith(color: AppColors.textSecondary),
+      body: Column(
+        children: [
+          MessageAppBar(
+            title: widget.contactName,
+            imagePath: widget.imagePath,
+          ),
+          Expanded(
+            child: messages.isEmpty
+                ? Center(
+                    child: Text(
+                      'No messages yet.',
+                      textAlign: TextAlign.center,
+                      style: AppTextStyles.textMD
+                          .copyWith(color: AppColors.textSecondary),
+                    ),
+                  )
+                : ListView.builder(
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(messages[index]),
+                      );
+                    },
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextInputField(
+              controller: messageController,
+              hint: "Message",
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
