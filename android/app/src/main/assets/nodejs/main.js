e6256f89e396a15b452e40fce16ac08f11722b1d
CalebCouch <calebblackcat@gmail.com>
2024-06-06 11:46:33 -0400

    Working nodejs for android with npm

---
+++ android/app/src/main/assets/nodejs/main.js
@@ -0,0 +1,6 @@
+const http = require('http');
+const versions_server = http.createServer( (request, response) => {
+  response.end('Versions: ' + JSON.stringify(process.versions));
+});
+versions_server.listen(3000);
+console.log('The node project has started.');
--- android/app/src/main/kotlin/com/example/orange/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/orange/MainActivity.kt
@@ -2,8 +2,82 @@ package com.example.orange
 
 import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
+import android.content.Context;
+import android.content.res.AssetManager;
+import java.io.*;
 
 class MainActivity: FlutterActivity() {
+
+    private fun deleteFolderRecursively(file: File): Boolean {
+        return try {
+            var res = true
+            file.listFiles()?.forEach { childFile ->
+                if (childFile.isDirectory) {
+                    res = res && deleteFolderRecursively(childFile)
+                } else {
+                    res = res && childFile.delete()
+                }
+            }
+            res = res && file.delete()
+            res
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    private fun copyAssetFolder(assetManager: AssetManager, fromAssetPath: String, toPath: String): Boolean {
+        return try {
+            val files = assetManager.list(fromAssetPath)
+            var res = true
+
+            if (files != null) {
+                if (files.isEmpty()) {
+                    // If it's a file, it won't have any assets "inside" it.
+                    res = copyAsset(assetManager, fromAssetPath, toPath)
+                } else {
+                    File(toPath).mkdirs()
+                    for (file in files) {
+                        res = res && copyAssetFolder(assetManager, "$fromAssetPath/$file", "$toPath/$file")
+                    }
+                }
+            }
+            res
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    private fun copyAsset(assetManager: AssetManager, fromAssetPath: String, toPath: String): Boolean {
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+        return try {
+            inputStream = assetManager.open(fromAssetPath)
+            File(toPath).createNewFile()
+            outputStream = FileOutputStream(toPath)
+            copyFile(inputStream, outputStream)
+            inputStream.close()
+            inputStream = null
+            outputStream.flush()
+            outputStream.close()
+            outputStream = null
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun copyFile(inputStream: InputStream, outputStream: OutputStream) {
+        val buffer = ByteArray(1024)
+        var read: Int
+        while (inputStream.read(buffer).also { read = it } != -1) {
+            outputStream.write(buffer, 0, read)
+        }
+    }
+
     init {
         System.loadLibrary("native-lib")
     }
@@ -17,14 +91,19 @@ class MainActivity: FlutterActivity() {
         if (!_startedNodeAlready) {
             _startedNodeAlready = true
             Thread(Runnable {
-                startNodeWithArguments(arrayOf("node", "-e", """
-                var http = require('http');
-                var versions_server = http.createServer((request, response) => {
-                  response.end('Versions: ' + JSON.stringify(process.versions));
-                });
-                versions_server.listen(3000);
-            """.trimIndent()))
+                // The path where we expect the node project to be at runtime.
+                val nodeDir = applicationContext.filesDir.absolutePath + "/nodejs"
+// Recursively delete any existing nodejs-project.
+                val nodeDirReference = File(nodeDir)
+                if (nodeDirReference.exists()) {
+                    deleteFolderRecursively(File(nodeDir))
+                }
+// Copy the node project from assets into the application's data path.
+                copyAssetFolder(applicationContext.assets, "nodejs", nodeDir)
+                startNodeWithArguments(arrayOf("node", "$nodeDir/main.js"))
             }).start()
         }
     }
 }
+
+
