41e535910cb9c2a09f6fc574b4bfeeed9db4dc2d
Jack <jack@sophtron.com>
2024-06-06 15:02:58 -0400

    WIP Ios Nodejs

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,19 @@
 import UIKit
 import Flutter
 
+
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+      //let thread = Thread.init(target: self, selector: #selector(NodeRunner.startNode), object: nil);
+      //thread.start()
+      
+      GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
