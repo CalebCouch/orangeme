4c685d92189390bd32e86b33183b9a4b5587bf6c
CalebCouch <calebblackcat@gmail.com>
2024-06-06 11:04:32 -0400

    working nodejs server with android

---
+++ android/app/.cxx/Debug/n6v4s232/armeabi-v7a/CMakeFiles/3.18.1-g262b901/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.5.0-35-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-35-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/shared/Android/Sdk/ndk/23.1.7779620/build/cmake/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-1")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "armv7-a")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
--- android/app/src/main/kotlin/com/example/orange/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/orange/MainActivity.kt
@@ -1,5 +1,30 @@
 package com.example.orange
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    init {
+        System.loadLibrary("native-lib")
+    }
+
+    external fun startNodeWithArguments(args: Array<String>): Void
+
+    var _startedNodeAlready: Boolean = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!_startedNodeAlready) {
+            _startedNodeAlready = true
+            Thread(Runnable {
+                startNodeWithArguments(arrayOf("node", "-e", """
+                var http = require('http');
+                var versions_server = http.createServer((request, response) => {
+                  response.end('Versions: ' + JSON.stringify(process.versions));
+                });
+                versions_server.listen(3000);
+            """.trimIndent()))
+            }).start()
+        }
+    }
+}
