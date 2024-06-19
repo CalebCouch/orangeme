c8730fe87bee905276b6f8fab4f578a881957269
dav-anderson <david@orange.me>
2024-06-19 16:12:01 -0400

    added function and static placeholder messages to the group message screen variant

--- lib/screens/social/group_message.dart
+++ lib/screens/social/group_message.dart
@@ -3,6 +3,8 @@ import 'package:orange/styles/constants.dart';
 import 'package:orange/widgets/message_appbar.dart';
 import 'package:orange/components/textfield.dart';
 import 'package:orange/screens/social/group_message_list.dart';
+import 'package:orange/widgets/text_bauble.dart';
+import 'package:flutter/rendering.dart';
 
 class MessageGroup extends StatefulWidget {
   final List<String> recipients;
@@ -19,7 +21,114 @@ class MessageGroup extends StatefulWidget {
 
 class MessageGroupState extends State<MessageGroup> {
   final TextEditingController messageController = TextEditingController();
-  List<String> messages = [];
+  final ScrollController scrollController = ScrollController();
+  bool submitEnabled = false;
+  List<Map<String, String>> messages = [
+    {
+      "message":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+      "incoming": "true",
+      "sender": 'Pam Beesley',
+      "timestamp": "2024-06-10 11:59:53"
+    },
+    {
+      "message":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+      "incoming": "false",
+      "sender": 'me',
+      "timestamp": "2024-06-11 13:32:09"
+    },
+    {
+      "message":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+      "incoming": "true",
+      "sender": 'Michael Scott',
+      "timestamp": "2024-06-11 14:01:26"
+    },
+    {
+      "message":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+      "incoming": "false",
+      "sender": "me",
+      "timestamp": "2024-06-12 12:29:09"
+    },
+  ];
+  @override
+  void initState() {
+    super.initState();
+    messageController.addListener(() {
+      if (messageController.text.isNotEmpty) {
+        setState(() {
+          if (validateMessage(messageController.text)) {
+            submitEnabled = true;
+          } else {
+            submitEnabled = false;
+          }
+        });
+      } else {
+        setState(() {
+          submitEnabled = false;
+        });
+      }
+    });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      scrollController.addListener(() {
+        print("scrolling");
+        if (scrollController.hasClients &&
+            scrollController.position.userScrollDirection ==
+                ScrollDirection.forward) {
+          print('scrolling up, diposing focus');
+          FocusScope.of(context).unfocus();
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void submitMessage() {
+    print("adding message");
+    String formattedTimestamp = DateTime.now().toString();
+    String formattedMessage = formatMessage(messageController.text);
+    setState(() {
+      messages.add({
+        "message": formattedMessage,
+        "incoming": "false",
+        "sender": 'me',
+        "timestamp": formattedTimestamp
+      });
+      messageController.text = '';
+      submitEnabled = false;
+      scrollToBottom();
+    });
+  }
+
+  String formatMessage(String input) {
+    String trimmed = input.trim();
+    String normalized = trimmed.replaceAll(RegExp(r'\n+'), '\n');
+    return normalized;
+  }
+
+  bool validateMessage(String input) {
+    return RegExp(r'[^\s]').hasMatch(input);
+  }
+
+  void scrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 200), () {
+      if (scrollController.hasClients) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
 
   void showParticipants() {
     print("Showing message participants");
@@ -42,29 +151,35 @@ class MessageGroupState extends State<MessageGroup> {
             showParticipants: showParticipants,
           ),
           Expanded(
-            child: messages.isEmpty
-                ? Center(
-                    child: Text(
-                      'No messages yet.',
-                      textAlign: TextAlign.center,
-                      style: AppTextStyles.textMD
-                          .copyWith(color: AppColors.textSecondary),
-                    ),
-                  )
-                : ListView.builder(
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(messages[index]),
-                      );
-                    },
-                  ),
-          ),
+              child: messages.isEmpty
+                  ? Center(
+                      child: Text(
+                        'No messages yet.',
+                        textAlign: TextAlign.center,
+                        style: AppTextStyles.textMD
+                            .copyWith(color: AppColors.textSecondary),
+                      ),
+                    )
+                  : ListView.builder(
+                      controller: scrollController,
+                      itemCount: messages.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return MessageBauble(
+                            message: messages[index]["message"]!,
+                            incoming: messages[index]["incoming"]!,
+                            timestamp: messages[index]["timestamp"]!,
+                            name: messages[index]["sender"]!);
+                      },
+                    )),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextInputField(
               controller: messageController,
               hint: "Message",
+              showSubmit: true,
+              onEditingComplete: submitMessage,
+              showNewLine: true,
+              submitEnabled: submitEnabled,
             ),
           ),
         ],
--- lib/widgets/text_bauble.dart
+++ lib/widgets/text_bauble.dart
@@ -48,7 +48,8 @@ class MessageBaubleState extends State<MessageBauble> {
     } else if (dayDifference <= 7) {
       dayResult = "${truncateDayName(time.weekday)} ";
     } else {
-      dayResult = "${time.month} ${time.day}, ${time.year}, ";
+      dayResult =
+          "${stringifyMonthName(time.month)} ${time.day}, ${time.year}, ";
     }
 
     String timeFormatted = DateFormat('h:mm a').format(time);
@@ -73,6 +74,36 @@ class MessageBaubleState extends State<MessageBauble> {
       return "Sat";
     } else if (weekday == 7) {
       return "Sun";
+    } else {
+      return '';
+    }
+  }
+
+  String stringifyMonthName(int month) {
+    if (month == 1) {
+      return "Jan";
+    } else if (month == 2) {
+      return "Feb";
+    } else if (month == 3) {
+      return "Mar";
+    } else if (month == 4) {
+      return "Apr";
+    } else if (month == 5) {
+      return "May";
+    } else if (month == 6) {
+      return "Jun";
+    } else if (month == 7) {
+      return "Jul";
+    } else if (month == 8) {
+      return "Aug";
+    } else if (month == 9) {
+      return "Sept";
+    } else if (month == 10) {
+      return "Oct";
+    } else if (month == 11) {
+      return "Nov";
+    } else if (month == 12) {
+      return "Dec";
     } else {
       return '';
     }
