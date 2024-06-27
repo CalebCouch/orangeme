709d6bf2e797f5e1a672203fe1540c84b5e84e20
dav-anderson <david@orange.me>
2024-06-27 12:14:59 -0400

    fixed some linter errors

--- lib/components/custom_qr_generator/qr_painter.dart
+++ lib/components/custom_qr_generator/qr_painter.dart
@@ -1,13 +1,8 @@
-import 'dart:convert';
 import 'dart:math';
-
 import 'custom_qr_generator.dart';
-import 'util.dart';
-import 'colors/color.dart';
-import 'options/options.dart';
 import 'package:flutter/widgets.dart';
+// ignore: depend_on_referenced_packages
 import 'package:zxing_lib/qrcode.dart';
-import 'package:zxing_lib/zxing.dart';
 
 class QrPainter extends CustomPainter {
   final String data;
@@ -36,8 +31,10 @@ class QrPainter extends CustomPainter {
     final padding = codeSize * options.padding / 3;
     final pixelSize = (codeSize - 2 * padding) / matrix.width;
     final realCodeSize = codeSize - padding * 2;
-    final lightPaint = options.colors.light.createPaint(realCodeSize, realCodeSize);
-    final darkPaint = options.colors.dark.createPaint(realCodeSize, realCodeSize);
+    final lightPaint =
+        options.colors.light.createPaint(realCodeSize, realCodeSize);
+    final darkPaint =
+        options.colors.dark.createPaint(realCodeSize, realCodeSize);
     Path? darkPath;
     Path? lightPath;
     Path fullDarkPath = Path();
@@ -46,11 +43,15 @@ class QrPainter extends CustomPainter {
     var frameSize = pixelSize * 7;
     var ballSize = pixelSize * 3;
 
-    final framePathZeroOffset = options.shapes.frame.createPath(Offset.zero, frameSize, Neighbors.empty);
-    final framePaint = options.colors.frame.createPaint(pixelSize * 3, pixelSize * 3);
+    final framePathZeroOffset = options.shapes.frame
+        .createPath(Offset.zero, frameSize, Neighbors.empty);
+    final framePaint =
+        options.colors.frame.createPaint(pixelSize * 3, pixelSize * 3);
 
-    final ballPathZeroOffset = options.shapes.ball.createPath(Offset.zero, pixelSize * 3, Neighbors.empty);
-    final ballPaint = options.colors.ball.createPaint(pixelSize * 3, pixelSize * 3);
+    final ballPathZeroOffset = options.shapes.ball
+        .createPath(Offset.zero, pixelSize * 3, Neighbors.empty);
+    final ballPaint =
+        options.colors.ball.createPaint(pixelSize * 3, pixelSize * 3);
 
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height),
         options.colors.background.createPaint(size.width, size.height));
@@ -65,7 +66,8 @@ class QrPainter extends CustomPainter {
         canvas.drawPath(framePathZeroOffset, framePaint);
         canvas.restore();
       } else {
-        var path = options.shapes.frame.createPath(Offset(dx, dy), frameSize, Neighbors.empty);
+        var path = options.shapes.frame
+            .createPath(Offset(dx, dy), frameSize, Neighbors.empty);
         canvas.drawPath(path, darkPaint);
       }
     }
@@ -77,7 +79,8 @@ class QrPainter extends CustomPainter {
         canvas.drawPath(ballPathZeroOffset, ballPaint);
         canvas.restore();
       } else {
-        var path = options.shapes.ball.createPath(Offset(dx, dy), ballSize, Neighbors.empty);
+        var path = options.shapes.ball
+            .createPath(Offset(dx, dy), ballSize, Neighbors.empty);
         canvas.drawPath(path, darkPaint);
       }
     }
@@ -91,34 +94,42 @@ class QrPainter extends CustomPainter {
     drawFrame(0, pixelSize * (matrix.width - 7));
     drawBall(pixelSize * 2, pixelSize * (matrix.height - 5));
 
-    int middleSize = (matrix.width / 3.4).floor();  // Make the center square bigger
+    int middleSize =
+        (matrix.width / 3.4).floor(); // Make the center square bigger
     int middleStart = (matrix.width - middleSize) ~/ 2;
     int middleEnd = middleStart + middleSize;
 
     for (int i = 0; i < matrix.width; i++) {
       for (int j = 0; j < matrix.height; j++) {
         // Skip drawing behind the eyes
-        if ((i < 7 && j < 7) || (i >= matrix.width - 7 && j < 7) || (i < 7 && j >= matrix.height - 7)) {
+        if ((i < 7 && j < 7) ||
+            (i >= matrix.width - 7 && j < 7) ||
+            (i < 7 && j >= matrix.height - 7)) {
           continue;
         }
 
         // Skip drawing within the middle square
-        if (i >= middleStart && i < middleEnd && j >= middleStart && j < middleEnd) {
+        if (i >= middleStart &&
+            i < middleEnd &&
+            j >= middleStart &&
+            j < middleEnd) {
           continue;
         }
 
         if (options.colors.dark is! QrColorUnspecified) {
-          darkPath = options.shapes.darkPixel.createPath(
-              Offset.zero, pixelSize, matrix.neighbors(i, j));
+          darkPath = options.shapes.darkPixel
+              .createPath(Offset.zero, pixelSize, matrix.neighbors(i, j));
           if (matrix.get(i, j) == 1) {
-            fullDarkPath.addPath(darkPath!, Offset(i * pixelSize, j * pixelSize));
+            fullDarkPath.addPath(
+                darkPath, Offset(i * pixelSize, j * pixelSize));
           }
         }
         if (options.colors.light is! QrColorUnspecified) {
-          lightPath = options.shapes.lightPixel.createPath(
-              Offset.zero, pixelSize, matrix.neighbors(i, j));
+          lightPath = options.shapes.lightPixel
+              .createPath(Offset.zero, pixelSize, matrix.neighbors(i, j));
           if (matrix.get(i, j) != 0) {
-            fullLightPath.addPath(lightPath!, Offset(i * pixelSize, j * pixelSize));
+            fullLightPath.addPath(
+                lightPath, Offset(i * pixelSize, j * pixelSize));
           }
         }
       }
