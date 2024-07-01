def6707752c0d3554711275a5a8080be3698fe09
elijahcouchorangeme <eli@orange.me>
2024-07-01 09:15:59 -0400

    fixing invoke methods and cleaning up code

--- lib/screens/non_premium/send3.dart
+++ lib/screens/non_premium/send3.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:orange/screens/non_premium/send4.dart';
 import 'package:orange/widgets/fee_selector.dart';
@@ -17,15 +18,14 @@ class Send3 extends StatefulWidget {
   final SessionTimerManager sessionTimer;
   final VoidCallback onDashboardPopBack;
 
-  Send3({
-    super.key,
-    required this.amount,
-    required this.address,
-    required this.balance,
-    required this.price,
-    required this.onDashboardPopBack,
-    required this.sessionTimer,
-  });
+  Send3(
+      {super.key,
+      required this.amount,
+      required this.address,
+      required this.balance,
+      required this.price,
+      required this.onDashboardPopBack,
+      required this.sessionTimer});
 
   @override
   Send3State createState() => Send3State();
@@ -39,26 +39,23 @@ class Send3State extends State<Send3> {
 
   void navigate() {
     Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => Send4(
-          tx: transaction,
-          balance: widget.balance,
-          amount: widget.amount,
-          price: widget.price,
-          onDashboardPopBack: widget.onDashboardPopBack,
-          sessionTimer: widget.sessionTimer,
-        ),
-      ),
-    );
+        context,
+        MaterialPageRoute(
+            builder: (context) => Send4(
+                tx: transaction,
+                balance: widget.balance,
+                amount: widget.amount,
+                price: widget.price,
+                onDashboardPopBack: widget.onDashboardPopBack,
+                sessionTimer: widget.sessionTimer)));
   }
 
   @override
   void initState() {
     print("initializing send3");
     super.initState();
-    createTransaction();
-    // Send the user back to the dashboard if the session expires
+  //  createTransaction();
+    //send the user back to the dashboard if the session expires
     widget.sessionTimer.setOnSessionEnd(() {
       if (mounted) {
         widget.onDashboardPopBack();
@@ -73,8 +70,8 @@ class Send3State extends State<Send3> {
     super.dispose();
   }
 
-  // Fired when user selects priority
-  // This currently does nothing other than change the visual indicator
+  //fired when user selects priority
+  //this currently does nothing other than change the visual indicator
   void onOptionSelected(bool isSelected) {
     setState(() {
       isPrioritySelected = isSelected;
@@ -82,53 +79,38 @@ class Send3State extends State<Send3> {
     });
   }
 
-  void createTransaction() async {
-    print("Address: ${widget.address}");
-    print("Amount: ${widget.amount}");
+void createTransaction() async {
+  print("Address: ${widget.address}");
+  print("Amount: ${widget.amount.toString()}");
 
-    var input = CreateTransactionInput(
-      widget.address.toString(),
-      widget.amount.toString(),
-      standardFee.toString(),
-    );
+  var input = CreateTransactionInput(widget.address.toString(), widget.amount.toString(), "80.00");
 
-    try {
-      var jsonRes = (await invoke("create_transaction", jsonEncode(input))).toString();
-      print("JSON Response: $jsonRes");
+  try {
+    var jsonRes = await invoke("create_transaction", jsonEncode(input));
+    print("JSON Response: $jsonRes");
 
-      if (!mounted) return;
+    if (!mounted) return;
 
-      if (jsonRes != null && jsonRes.trim().isNotEmpty) {
-        try {
-          var json = jsonDecode(jsonRes);
-          print("create tx response: $json");
+    if (jsonRes != null && jsonRes.toString().trim() != '') {
+      var json = jsonDecode(jsonRes.toString());
+      print("create tx response: $json");
 
-          calculateStandardFee(jsonRes);
+      calculateStandardFee(json.toString());
 
-          setState(() {
-            transaction = jsonRes;
-          });
-        } catch (e) {
-          print("Error decoding JSON: $e");
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("Error decoding JSON response. Please try again.")),
-          );
-        }
-      } else {
-        print("Error: Received empty or null JSON response");
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Received empty or null JSON response. Please try again.")),
-        );
-      }
-    } catch (e) {
-      print("Error invoking API: $e");
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error invoking API. Please check your connection and try again.")),
-      );
+      setState(() {
+        transaction = json.toString();
+      });
+    } else {
+      print("Error: Received empty or null JSON response");
     }
+  } catch (e) {
+    print("Error decoding JSON: $e");
   }
+}
 
-  // Calculate a standard fee as returned by create_transaction with no fee specified
+
+
+  //calcuate a standard fee as returned by create_transaction with no fee specified
   void calculateStandardFee(String transaction) {
     final transactionDecoded = Transaction.fromJson(jsonDecode(transaction));
     setState(() {
@@ -141,7 +123,7 @@ class Send3State extends State<Send3> {
     print("Time left ${widget.sessionTimer.getTimeLeftFormatted()}");
     return PopScope(
       canPop: true,
-      // Prevents session timer from continuing to run off screen
+      //prevents session timer from continuing to run off screen
       onPopInvoked: (bool didPop) async {
         widget.sessionTimer.dispose();
       },
@@ -149,24 +131,20 @@ class Send3State extends State<Send3> {
         appBar: AppBar(
           title: const Text('Transaction Speed'),
           leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              // Dashboard timer callback function
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Send2(
-                    amount: widget.amount,
-                    balance: widget.balance,
-                    price: widget.price,
-                    onDashboardPopBack: widget.onDashboardPopBack,
-                    sessionTimer: widget.sessionTimer,
-                    address: widget.address,
-                  ),
-                ),
-              );
-            },
-          ),
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                //dashboard timer callback function
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Send2(
+                            amount: widget.amount,
+                            balance: widget.balance,
+                            price: widget.price,
+                            onDashboardPopBack: widget.onDashboardPopBack,
+                            sessionTimer: widget.sessionTimer,
+                            address: widget.address)));
+              }),
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
@@ -175,10 +153,9 @@ class Send3State extends State<Send3> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               FeeSelector(
-                onOptionSelected: onOptionSelected,
-                price: widget.price,
-                standardFee: standardFee,
-              ),
+                  onOptionSelected: onOptionSelected,
+                  price: widget.price,
+                  standardFee: standardFee),
               const Spacer(),
             ],
           ),
