6558b6c2138962e15a729366676ed84b14c893e1
elijahcouchorangeme <eli@orange.me>
2024-07-02 12:31:01 -0400

    fixing invoke methods and cleaning up code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:orange/screens/error.dart';
 import 'package:orange/src/rust/api/simple.dart';
 import 'package:orange/src/rust/frb_generated.dart';
 import 'package:orange/screens/init.dart';
@@ -13,53 +12,28 @@ import 'dart:convert';
 import 'dart:math';
 import 'dart:io';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+Future<void> main() async {
   await RustLib.init();
   runApp(const MyApp());
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  var path = await getDocPath();
+  print(path);
+  ERROR = await rustStart(path: path, dartCallback: dartCallback);
+  print(ERROR);
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ValueNotifier<String> errorNotifier = ValueNotifier<String>("");
-
-  @override
-  void initState() {
-    super.initState();
-    setupApp();
-  }
-
-  Future<void> setupApp() async {
-    var path = await getDocPath();
-    print(path);
-    var _error = await rustStart(path: path, dartCallback: dartCallback);
-    errorNotifier.value = _error;
-    print(errorNotifier.value);
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Orange',
-      theme: theme(),
-      home: ValueListenableBuilder<String>(
-        valueListenable: errorNotifier,
-        builder: (context, error, _) {
-          if (error == "") {
-            return InitPage();
-          }
-          return ErrorPage(message: error);
-        },
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Orange',
+        theme: theme(),
+        home: const InitPage());
   }
 }
