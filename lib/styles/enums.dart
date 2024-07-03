1f5a31348bffc8f29dc7a68f551d67a5c8757df5
dav-anderson <david@orange.me>
2024-07-03 16:41:16 -0400

    enums

---
+++ lib/styles/enums.dart
@@ -0,0 +1,20 @@
+class Size {
+  final int value;
+  const Size._(this.value);
+
+  static const Size XXL = Size._(96);
+  static const Size XL = Size._(64);
+  static const Size LG = Size._(48);
+}
+
+class HeadingSize {
+  final int value;
+  const HeadingSize._(this.value);
+
+  static const HeadingSize h1 = HeadingSize._(48);
+  static const HeadingSize h2 = HeadingSize._(32);
+  static const HeadingSize h3 = HeadingSize._(24);
+  static const HeadingSize h4 = HeadingSize._(20);
+  static const HeadingSize h5 = HeadingSize._(16);
+  static const HeadingSize h6 = HeadingSize._(14);
+}
