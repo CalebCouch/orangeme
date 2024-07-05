6e6535d36b3880089f3568c6cbb7b11e2316ded2
EllaCouch20 <ella@orange.me>
2024-07-05 10:46:10 -0400

    padding on value display

--- lib/theme/stylesheet.dart
+++ lib/theme/stylesheet.dart
@@ -136,7 +136,7 @@ class AppPadding {
   static const double content = 24;
   static const double bumper = 16;
   static const double valueDisplaySep = 8;
-  static const double valueDisplay = 64;
+  static const double valueDisplay = 32;
   static const double navBar = 64;
   static const double listItem = 16;
   static const double textInput = 12;
