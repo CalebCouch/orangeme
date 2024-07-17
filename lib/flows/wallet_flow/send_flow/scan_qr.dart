b2e8411514372e2bf4c564d1436943dce9f859f6
EllaCouch20 <ella@orange.me>
2024-07-17 14:34:11 +0200

    qrcode

--- lib/flows/wallet_flow/send_flow/scan_qr.dart
+++ lib/flows/wallet_flow/send_flow/scan_qr.dart
@@ -6,6 +6,7 @@ import 'package:orange/components/content.dart';
 import 'package:orange/components/header.dart';
 import 'package:orange/components/custom/custom_text.dart';
 import 'package:orange/classes.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ScanQR extends StatefulWidget {
   final GlobalState globalState;
@@ -16,6 +17,15 @@ class ScanQR extends StatefulWidget {
 }
 
 class ScanQRState extends State<ScanQR> {
+  late QRViewController controller;
+  GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
@@ -44,8 +54,12 @@ class ScanQRState extends State<ScanQR> {
                 border: Border.all(color: ThemeColor.bgSecondary, width: 4),
                 borderRadius: const BorderRadius.all(Radius.circular(8)),
               ),
+              child: QRView(
+                key: qrKey,
+                onQRViewCreated: _onQRViewCreated,
+              ),
             ),
-            const Spacing(height: AppPadding.content),
+            const SizedBox(height: AppPadding.content),
             const CustomText(
               text: 'Scan a bitcoin QR code',
               color: ThemeColor.textSecondary,
@@ -55,5 +69,14 @@ class ScanQRState extends State<ScanQR> {
         ),
       ),
     );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        print(scanData.code);
+      });
+    });
   }
 }
