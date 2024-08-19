4761a50efee9f996e6011e07039214f59a6256bd
EllaCouch20 <ella@orange.me>
2024-08-19 13:17:14 +0200

    getprice error handling

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -91,9 +91,6 @@ struct Spot {amount: String, currency: String, base: String}
 #[derive(Deserialize)]
 struct SpotRes {
     data: Option<Spot>,
-    error: Option<String>,
-    code: Option<i32>,
-    message: Option<String>
 }
 //PRICE FETCH
 
@@ -245,18 +242,23 @@ struct Data {
 
 async fn get_price(callback: impl Fn(String) -> DartFnFuture<String>, prices: &mut SqliteStore, timestamp: u64) -> Result<f64, Error> {
     Ok(match prices.get(&timestamp.to_le_bytes())? {
-        Some(price) => f64::from_le_bytes(price.try_into().or(Err(Error::error("get_price", "Price not f64 bytes")))?),
+        Some(price) => f64::from_le_bytes(price.try_into().or(Err(Error::err("get_price", "Price not f64 bytes")))?),
         None => {
-            invoke(&callback, "print", "get_price:NONE").await?;
+            //invoke(&callback, "print", "get_price:NONE").await?;
             let error = Error::bad_request("Prices.get_price", "Invalid timestamp");
             let base_url = "https://api.coinbase.com/v2/prices/BTC-USD/spot";
             let date = DateTime::from_timestamp(timestamp as i64, 0).ok_or(error)?.format("%Y-%m-%d").to_string();
             let url = format!("{}?date={}", base_url, date);
-            invoke(&callback, "print", &format!("get_price:PRE{}", url)).await?;
+            //invoke(&callback, "print", &format!("get_price:PRE{}", url)).await?;
             let reqwest = reqwest::get(&url).await?;
-            invoke(&callback, "print", "reqwest").await?;
-            let spot_res: SpotRes = reqwest.json().await?;
-            let price = spot_res.data.ok_or(Error::Error(spot_res.error.unwrap(), spot_res.message.unwrap()))?.amount.parse::<f64>()?;
+            //invoke(&callback, "print", "reqwest").await?;
+            
+            let value: serde_json::Value = reqwest.json().await?;
+            let spot_res: SpotRes = serde_json::from_value(value.clone())?;
+            
+            //let spot_res: SpotRes = reqwest.json().await?;
+            let price = spot_res.data.ok_or(Error::err("Fetching price from coinbases api", &serde_json::to_string(&value)?))?.amount.parse::<f64>()?;
+            //invoke(&callback, "print", "xyzj").await?;
             prices.set(&timestamp.to_le_bytes(), &price.to_le_bytes())?;
             price
         }
@@ -285,16 +287,17 @@ async fn price_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'stati
 }
 
 async fn state_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'static + Sync + Send, wallet_path: PathBuf, store_path: PathBuf, price_path: PathBuf, descriptors: DescriptorSet, client_uri: String) -> Result<(), Error> {
-    //invoke(&callback, "print", "State Thread Running").await?;
+    //invoke(&callback, "print", "a").await?;
     let mut store = SqliteStore::new(store_path)?;
     let mut price = SqliteStore::new(price_path)?;
     let wallet = Wallet::new(&descriptors.external, Some(&descriptors.internal), Network::Bitcoin, SqliteDatabase::new(wallet_path.join("bdk.db")))?;
     let blockchain = ElectrumBlockchain::from(Client::new(&client_uri)?);
     loop {
+       //invoke(&callback, "print", "b").await?;
         //invoke(&callback, "print", "State Thread Looping").await?;
         let wallet_transactions = wallet.list_transactions(true)?;
         let balance = wallet.get_balance()?;
-        let current_price = price.get(b"price")?.map(|b| Ok::<f64, Error>(f64::from_le_bytes(b.try_into().or(Err(Error::error("Main", "Price not f64 bytes")))?))).unwrap_or(Ok(0.0))?;
+        let current_price = price.get(b"price")?.map(|b| Ok::<f64, Error>(f64::from_le_bytes(b.try_into().or(Err(Error::err("Main", "Price not f64 bytes")))?))).unwrap_or(Ok(0.0))?;
         let btc = balance.get_total() as f64 / SATS;
         let mut transactions: Vec<Transaction> = Vec::new();
 
@@ -330,16 +333,18 @@ async fn state_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'stati
                 members: vec![josh_thayer.clone(), chris_slaughter.clone(), ella_couch.clone()]
             }
         ];
-
+       //invoke(&callback, "print", "c").await?;
         let users: Vec<Contact> = vec![josh_thayer.clone(), ella_couch.clone(), chris_slaughter.clone(), jw_weatherman.clone(), josh_thayer_alt.clone(), ella_couch_alt.clone(), chris_slaughter_alt.clone(), jw_weatherman_alt.clone()];
 
         let personal: Contact = ella_couch.clone();
 
         for tx in wallet_transactions {
+           // invoke(&callback, "print", "tx a").await?;
             let price = match tx.confirmation_time.as_ref() {
                 Some(ct) => get_price(&callback, &mut price, ct.timestamp).await?,
                 None => current_price
             };
+           // invoke(&callback, "print", "tx c").await?;
             transactions.push(Transaction::from_details(tx, price, |s: &Script| {wallet.is_mine(s).unwrap_or(false)})?);
         }
 
@@ -354,11 +359,12 @@ async fn state_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'stati
             users,
             personal,
         };
-
+       // invoke(&callback, "print", "d").await?;
         store.set(b"state", &serde_json::to_vec(&state)?)?;
-        invoke(&callback, "set_state", &serde_json::to_string(&state)?).await?;
+       // invoke(&callback, "set_state", &serde_json::to_string(&state)?).await?;
         thread::sleep(time::Duration::from_millis(1000));
     }
+    invoke(&callback, "print", "e").await?;
     Err(Error::Exited(format!("Refresh Dart State Exited")))
 }
 
@@ -374,7 +380,7 @@ async fn command_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'sta
         for command in commands {
             let result: Result<String, Error> = match command.method.as_str() {
                 "get_new_address" => {
-                    Ok(String::from_utf8(store.get(b"new_address")?.ok_or(Error::error("Main.get_new_address", "No new address"))?)?)
+                    Ok(String::from_utf8(store.get(b"new_address")?.ok_or(Error::err("Main.get_new_address", "No new address"))?)?)
                 },
                 "check_address" => {
                     Ok(Address::from_str(&command.data).map(|a|
@@ -400,12 +406,12 @@ async fn command_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'sta
                     let (mut psbt, mut tx_details) = {
                         let mut builder = wallet.build_tx();
                         builder.add_recipient(address.script_pubkey(), amount);
-                        builder.fee_rate(FeeRate::from_btc_per_kvb(fees[0] as f32));
+                        builder.fee_rate(FeeRate::from_btc_per_kvb(blockchain.estimate_fee(3)? as f32));
                         builder.finish()?
                     };
 
                     let finalized = wallet.sign(&mut psbt, SignOptions::default())?;
-                    if !finalized { return Err(Error::error(ec, "Could not sign std tx"));}
+                    if !finalized { return Err(Error::err(ec, "Could not sign std tx"));}
 
                     let tx = psbt.clone().extract_tx();
                     let mut stream: Vec<u8> = Vec::new();
@@ -448,21 +454,22 @@ async fn command_thread(callback: impl Fn(String) -> DartFnFuture<String> + 'sta
     }
     Err(Error::Exited("Main Exited".to_string()))
 }
-
 async fn flatten(handle: JoinHandle<Result<(), Error>>) -> Result<(), Error> {
     match handle.await {
         Ok(Ok(o)) => Ok(o),
         Ok(Err(err)) => Err(err),
         Err(e) => match e.try_into_panic() {
-            Ok(panic) => match panic.downcast_ref::<String>() {
-                Some(p) => Err(Error::Exited(p.to_string())),
-                None => Err(Error::Exited("Cannot convert panic to string".to_string())),
+            Ok(panic) => match panic.downcast_ref::<Box<dyn Send + 'static + std::fmt::Debug>>() {
+                Some(p) => Err(Error::Exited(format!("{:?}", p))),
+                None => match panic.downcast_ref::<Box<dyn Send + 'static + std::fmt::Display>>() {
+                    Some(p) => Err(Error::Exited(p.to_string())),
+                    None => Err(Error::Exited(format!("Cannot convert panic with type id {:?} to string", panic.type_id()))),
+                }
             },
             Err(e) => Err(Error::Exited(e.to_string()))
         }
     }
 }
-
 pub async fn rustStart (
     path: String,
     callback: impl Fn(String) -> DartFnFuture<String> + 'static + Sync + Send,
