8d233a59a0d14334ef80cb90ff9110a13eacfa8d
EllaCouch20 <ella@orange.me>
2024-10-07 21:20:40 -0700

    updated error screen

--- lib/screens/error.dart
+++ lib/screens/error.dart
@@ -13,26 +13,24 @@ class ErrorPage extends StatefulWidget {
 class ErrorPageState extends State<ErrorPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Error'),
-      ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: constraints.maxHeight,
-              ),
-              child: IntrinsicHeight(
-                child: Center(
-                  child: CustomText('text md', "error message: ${widget.message}"),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
+    return Stack_Scroll(
+      Header_Stack(context, 'Uh-oh!'),
+      [
+        const CustomText('heading h4 heading', 'Looks like we\'ve hit a snag'),
+        Image.asset('assets/images/error.png'),
+        const CustomText('text lg heading', 'Please try again or check your connection.'),
+        CustomButton('Contact Support', 'secondary md enabled hug none', () {}),
+        const CustomText('text md text_secondary', 'Error Message:'),
+        Container(
+          decoration: BoxDecoration(
+            color: ThemeColor.bgSecondary,
+            border: Border.all(color: ThemeColor.border),
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+          ),
+          padding: const EdgeInsets.all(12),
+          child: CustomText('text lg heading', widget.message),
+        ),
+      ],
     );
   }
 }
