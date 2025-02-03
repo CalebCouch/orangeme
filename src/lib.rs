250061e28af92bfe233adc533ddfebf7563a29d3
CalebCouch <calebblackcat@gmail.com>
2025-02-03 15:57:34 -0500

    Color code check

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,37 @@ use rust_on_rails2::*;
 pub struct MyApp {}
 
 impl App for MyApp {
-    const LOG_LEVEL: LogLevel = LogLevel::Info;
+    //const LOG_LEVEL: LogLevel = LogLevel::Info;
 
     async fn new() -> Self {
     //  let fonts = HashMap::from([("bold".to_string(), include_bytes!("../assets/outfit_bold.ttf").to_vec())]);
         MyApp{}
     }
 
-    async fn draw(&mut self) -> Vec<Mesh> {
-        vec![Mesh{
-            shape: Shape::Rectangle(100, 100),
-            offset: (100, 100)
-        }]
+    async fn draw(&mut self, width: u32, height: u32) -> Vec<Mesh> {
+        println!("width: {}", width);
+        vec![
+            Mesh{
+                shape: Shape::Rectangle(width-10, height-10),
+                offset: (5, 5),
+                color: "000000"
+            },
+          //Mesh{
+          //    shape: Shape::RoundedRectangle(100, 100, 100),
+          //    offset: (500, 300),
+          //    color: "ff0000"
+          //},
+          //Mesh{
+          //    shape: Shape::Rectangle(200, 300),
+          //    offset: (400, 500),
+          //    color: "0000ff"
+          //},
+            Mesh{
+                shape: Shape::Circle(100),
+                offset: (200, 200),
+                color: "eb343a"
+            },
+        ]
     }
 }
 
