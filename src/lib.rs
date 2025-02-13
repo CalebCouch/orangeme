93dc9cbf25fcde0a6a5baead1c30ca9c2bf6663d
Ella Mae <ella@orange.me>
2025-02-13 16:31:00 -0700

    broken scissor rect

--- src/lib.rs
+++ src/lib.rs
@@ -1,121 +1,4 @@
-use rust_on_rails::prelude::*;
-use rust_on_rails::prelude::Text as BasicText;
-
-pub struct Text(pub BasicText);
-impl Text {
-    pub fn new(text: &'static str, color: &'static str, size: u32, font: Handle) -> Self {
-        Text(BasicText(text, color, size, (size as f32*1.25) as u32, font))
-    }
-}
-
-impl ComponentBuilder for Text {
-    fn build_children(&self, ctx: &mut ComponentContext, max_size: Vec2) -> Vec<Box<dyn Drawable>> {
-        self.0.build_children(ctx, max_size)
-    }
-
-    fn on_click(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-        self.0.0 = ("T".to_string()+self.0.0).leak()
-    }
-
-    fn on_move(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-    }
-}
-
-
-pub struct Column(pub Vec<Box<dyn ComponentBuilder>>, pub u32);
-
-#[macro_export]
-macro_rules! Column {
-    ($x:literal, $($i:expr),*) => {{
-        Column(vec![
-            $(Box::new($i) as Box<dyn ComponentBuilder>),*
-        ], $x)
-    }}
-}
-
-impl ComponentBuilder for Column {
-    fn build_children(&self, ctx: &mut ComponentContext, max_size: Vec2) -> Vec<Box<dyn Drawable>> {
-        let mut bound = Rect::new(0, 0, max_size.x, max_size.y);
-        self.0.iter().map(|builder| {
-            let child = builder.build(ctx, bound);
-            let height = child.size(ctx).y;
-            bound.h -= self.1 + height;
-            bound.y += self.1 + height;
-            Box::new(child) as Box<dyn Drawable>
-        }).collect()
-    }
-
-    fn on_click(&mut self, ctx: &mut ComponentContext, max_size: Vec2, position: Vec2) {
-        let mut bound = Rect::new(0, 0, max_size.x, max_size.y);
-        for builder in &mut self.0 {
-            let child = builder.build(ctx, bound);
-            let size = child.size(ctx);
-            if size.x > position.x && bound.y+size.y > position.y {
-                builder.on_click(
-                    ctx,
-                    Vec2::new(max_size.x, bound.y+self.1),
-                    Vec2::new(max_size.x, position.y-bound.y)
-                );
-                break;
-            }
-            if bound.y+size.y+self.1 > position.y {
-                break;//Clicked to the side or inbetween objects
-            }
-            bound.h -= self.1 + size.y;
-            bound.y += self.1 + size.y;
-        }
-    }
-
-    fn on_move(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-    }
-}
-
-pub struct FText(Text, Handle);
-impl FText {
-    pub fn new(text: &'static str, color: &'static str, size: u32, font: Handle, ofont: Handle) -> Self {
-        FText(Text::new(text, color, size, font), ofont)
-    }
-}
-
-impl ComponentBuilder for FText {
-    fn build_children(&self, ctx: &mut ComponentContext, max_size: Vec2) -> Vec<Box<dyn Drawable>> {
-        self.0.build_children(ctx, max_size)//Pipe children
-    }
-
-    fn on_click(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-        let ohandle = self.0.0.4.clone();
-        self.0.0.4 = self.1.clone();
-        self.1 = ohandle;
-    }
-
-
-    fn on_move(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-    }
-}
-
-pub struct FShape(Shape);
-impl FShape {
-    pub fn new(shape: ShapeType, color: &'static str, stroke_width: Option<u16>) -> Self {
-        FShape(Shape(shape, color, stroke_width))
-    }
-}
-
-impl ComponentBuilder for FShape {
-    fn build_children(&self, ctx: &mut ComponentContext, max_size: Vec2) -> Vec<Box<dyn Drawable>> {
-        self.0.build_children(ctx, max_size)//Pipe children
-    }
-
-    fn on_click(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-        if self.0.1 == "ff0000" {self.0.1 = "00ff00"}
-        else if self.0.1 == "00ff00" {self.0.1 = "0000ff"}
-        else {self.0.1 = "ff0000"}
-    }
-
-
-    fn on_move(&mut self, _ctx: &mut ComponentContext, _max_size: Vec2, _position: Vec2) {
-    }
-}
-
+use pelican_ui::prelude::*;
 
 pub struct MyApp;
 
@@ -126,17 +9,20 @@ impl App for MyApp {
         let ofont = ctx.load_font("fonts/outfit_regular.ttf").unwrap();
         let image = ctx.load_image("icons/pfp.png").unwrap();
         Box::new(Column!(20,
-            Text::new("HELLO", "eb343a", 48, font.clone()),
-            FText::new("WORLD", "eb343a", 48, font, ofont),
-            FShape::new(ShapeType::Rectangle(100, 100), "00ff00", None),
-            FShape::new(ShapeType::Rectangle(100, 100), "00ff00", Some(500)),
-            Image(ShapeType::Rectangle(100, 100), image)
+            // Text::new("HELLO", "eb343a", 48, font.clone()),
+            // FText::new("WORLD", "eb343a", 48, font, ofont),
+            // FShape::new(ShapeType::Rectangle(100, 100), "00ff00", None),
+            // FShape::new(ShapeType::Rectangle(100, 100), "00ff00", Some(500)),
+            Image(ShapeType::Rectangle(8, 8), image),
+            Button::primary("Continue", Size::Large, Width::Expand),
+            Row!(32, Button::secondary("Continue", Size::Medium, Width::Hug),
+            Button::primary("Continue", Size::Large, Width::Hug))
+            // Stack!(Button::primary("Continue", Size::Large), Button::secondary("Continue", Size::Medium))
         ))
     }
 }
 
 create_entry_points!(MyApp);
-
 
 //  use rust_on_rails::prelude::*;
 
