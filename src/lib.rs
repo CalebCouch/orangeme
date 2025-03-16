f30e9bc40a106d849db0fadea7c219eaa09f5779
CalebCouch <calebblackcat@gmail.com>
2025-03-16 09:26:16 -0400

    test

--- src/lib.rs
+++ src/lib.rs
@@ -32,11 +32,11 @@ impl App for MyApp {
         let text = CanvasItem::Text(Text::new("HELLO WORLD", "eb343a", 255, None, 48, 60, font.clone()));
         //let shape = CanvasItem::Shape(Shape::Ellipse(0, (200, 100)), "ff00bb", 255);
         //let rectangle = CanvasItem::Image(Shape::Rectangle(0, (1000, 1000)), image.clone());
-        let circle = CanvasItem::Image(Shape::RoundedRectangle(2, (100, 100), 20), image.clone());
-        let circle2 = CanvasItem::Image(Shape::RoundedRectangle(4, (200, 100), 50), image.clone());
-        let circle3 = CanvasItem::Image(Shape::RoundedRectangle(2, (200, 400), 0), image.clone());
-        let circle4 = CanvasItem::Image(Shape::RoundedRectangle(20, (200, 400), 5), image.clone());
-        let circle5 = CanvasItem::Image(Shape::RoundedRectangle(0, (200, 400), 50), image);
+        let circle = CanvasItem::Shape(Shape::Ellipse(2, (100, 100)), "ff00ff", 255);
+        let circle2 = CanvasItem::Shape(Shape::Rectangle(4, (200, 100)), "00ff00", 255);
+        let circle3 = CanvasItem::Shape(Shape::RoundedRectangle(2, (200, 400), 0), "000000", 128);
+        let circle4 = CanvasItem::Shape(Shape::RoundedRectangle(20, (200, 400), 5), "ff0000", 200);
+        let circle5 = CanvasItem::Shape(Shape::RoundedRectangle(0, (200, 400), 50), "000000", 20);
 
         //items.push((Area((0, 0), None), text));
         //items.push((Area((200, 200), None), shape));
