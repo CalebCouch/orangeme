c137de6167d7858ff868fb90d3fd6f6104c37024
Ella Couch <ella@orange.me>
2025-03-22 16:57:03 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use rust_on_rails::prelude::*;
 pub struct MyApp;
 
 impl App for MyApp {
-    async fn new(ctx: &mut Context<'_>) -> Box<dyn Component> {
+    async fn new(ctx: &mut Context<'_>) -> Box<dyn Drawable> {
         let mut plugin = PelicanUI::init(ctx);
         // let mut theme = Theme::default(ctx);
         
@@ -40,21 +40,24 @@ impl App for MyApp {
 
         let image = resources::Image::new(ctx, image::load_from_memory(include_bytes!("../assets/icons/profile.png")).unwrap().into());
 
-        let theme = &ctx.get::<PelicanUI>().theme;
+        let color = ctx.get::<PelicanUI>().theme.colors.brand.secondary;
 
-        Box::new(Padding((24, 24), Box::new(Column!(24,
-            Button::primary("Continue"),
-            Button::secondary(Some("paste"), "Paste Clipboard", None),
-            // pelican_ui::prelude::Text(TextStyle::Heading, "Bitcoin received", 24),
-            // pelican_ui::prelude::Text(TextStyle::Secondary, "Bitcoin sent to the wrong address can never be recovered.", 16),
-            // Row!(24, Icon("error", o, 16), pelican_ui::prelude::Text(TextStyle::Error, "Not enough bitcoin.", 16)),
-            CircleIcon::Icon(CircleIconData("door", CircleIconStyle::Secondary), Some(CircleIconData("edit", CircleIconStyle::Secondary)), false, 128),
-            CircleIcon::Icon(CircleIconData("wallet", CircleIconStyle::Brand), None, false, 128),
-            CircleIcon::Image(image, Some(CircleIconData("edit", CircleIconStyle::Secondary)), false, 128),
-            // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("checkmark", CircleIconStyle::Success)), false, 128),
-            // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("microphone", CircleIconStyle::Danger)), false, 128),
-            // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("warning", CircleIconStyle::Warning)), false, 128)
-        ))))
+        // Box::new(Padding((24, 24), Box::new(Column!(24,
+        //     // Button::primary("Continue"),
+        //     // Button::secondary(Some("paste"), "Paste Clipboard", None),
+        //     // pelican_ui::prelude::Text(TextStyle::Heading, "Bitcoin received", 24),
+        //     // pelican_ui::prelude::Text(TextStyle::Secondary, "Bitcoin sent to the wrong address can never be recovered.", 16),
+        //     // Row!(24, Icon("error", o, 16), pelican_ui::prelude::Text(TextStyle::Error, "Not enough bitcoin.", 16)),
+        //     // CircleIcon::Icon(CircleIconData("door", CircleIconStyle::Secondary), Some(CircleIconData("edit", CircleIconStyle::Secondary)), false, 128),
+        //     // CircleIcon::Image(image, Some(CircleIconData("edit", CircleIconStyle::Secondary)), false, 128),
+        //     // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("checkmark", CircleIconStyle::Success)), false, 128),
+        //     // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("microphone", CircleIconStyle::Danger)), false, 128),
+        //     // CircleIcon(ProfileImage::Icon(CircleIconData("profile", CircleIconStyle::Secondary)), Some(CircleIconData("warning", CircleIconStyle::Warning)), false, 128)
+        // ))))
+
+        // Box::new(Icon::new(ctx, "error", color, 128))
+        Box::new(CircleIcon::new(ctx, ProfileImage::Icon("wallet", CircleIconStyle::Brand), Some(("edit", CircleIconStyle::Secondary)), false, 128))
+        // Box::new(CircleIconData::new(ctx, "wallet", CircleIconStyle::Brand, 128))
     }
 }
 
