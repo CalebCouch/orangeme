01b19ef944b026a19efd5e44a4eff736af4b2ea9
CalebCouch <calebblackcat@gmail.com>
2025-03-24 14:03:57 -0400

    test

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,6 @@ use rust_on_rails::prelude::*;
 use resources::Font;
 //use pelican_ui::prelude::*;
 
-//  #[derive(Clone)]
-//  pub struct Column(pub u32, pub Vec<BoxComponent>);
-
-//  impl LayoutComponent for Column {
-//      fn children(&self) -> Vec<&BoxComponent> {self.1.iter().collect()}
-//      fn children_mut(&self) -> Vec<&mut BoxComponent> {self.1.iter_mut().collect()}
-
-//      fn layout(&self, ctx: &mut , max_size: (u32, u32), items: Vec<fn (u32, u32) -> (u32, u32)>) -> Vec<((i32, i32), (u32, u32))> {
-//          let mut offset = 0;
-//          items.iter_mut().flat_map(|c| {
-//              if max_size.1 < offset {None} else {
-//                  //let size = c.size(ctx, (max_size.0, max_size.1-offset));
-//                  let size = todo!();
-//                  let v = ((0, offset as i32), size);
-//                  offset += size.1+self.0;
-//                  v
-//              }
-//          }).collect()
-
-//      }
-//  }
-
 #[derive(Clone, Copy, Default, Debug)]
 pub enum Size {
     #[default]
@@ -122,9 +100,7 @@ pub struct Column(pub u32, pub Offset, pub Size);
 impl Column {
     pub fn get_size(size: &Size, items: Vec<Option<u32>>) -> Option<u32> {
         match size {
-            Size::Fit => items.into_iter().fold(Some(0),
-                |os, s| os.and_then(|os| s.map(|s| os.max(s)))
-            ),
+            Size::Fit => items.into_iter().try_fold(0, |os, s| s.map(|s| os.max(s))),
             Size::Fill => None,
             Size::Static(s) => Some(*s)
         }
@@ -133,7 +109,7 @@ impl Column {
 
 
 impl Layout for Column {
-    fn build(&self, ctx: &mut Context, max_size: (u32, u32), items: Vec<(Option<u32>, Option<u32>)>) -> Vec<((i32, i32), (u32, u32))> {
+    fn build(&self, _ctx: &mut Context, max_size: (u32, u32), items: Vec<(Option<u32>, Option<u32>)>) -> Vec<((i32, i32), (u32, u32))> {
         let (widths, heights): (Vec<Option<u32>>, Vec<Option<u32>>) = items.iter().cloned().unzip();
 
         let col_width = Self::get_size(&self.2, widths).unwrap_or(max_size.0);
@@ -151,7 +127,7 @@ impl Layout for Column {
         }).collect()
     }
 
-    fn size(&self, ctx: &mut Context, items: Vec<(Option<u32>, Option<u32>)>) -> (Option<u32>, Option<u32>) {
+    fn size(&self, _ctx: &mut Context, items: Vec<(Option<u32>, Option<u32>)>) -> (Option<u32>, Option<u32>) {
         let (widths, heights): (Vec<_>, Vec<_>) = items.into_iter().unzip();
         let items = heights.len();
         (
@@ -167,9 +143,7 @@ pub struct Row(pub u32, pub Offset, pub Size);
 impl Row {
     pub fn get_size(size: &Size, items: Vec<Option<u32>>) -> Option<u32> {
         match size {
-            Size::Fit => items.into_iter().fold(Some(0),
-                |os, s| os.and_then(|os| s.map(|s| os.max(s)))
-            ),
+            Size::Fit => items.into_iter().try_fold(0, |os, s| s.map(|s| os.max(s))),
             Size::Fill => None,
             Size::Static(s) => Some(*s)
         }
@@ -178,7 +152,7 @@ impl Row {
 
 
 impl Layout for Row {
-    fn build(&self, ctx: &mut Context, max_size: (u32, u32), items: Vec<(Option<u32>, Option<u32>)>) -> Vec<((i32, i32), (u32, u32))> {
+    fn build(&self, _ctx: &mut Context, max_size: (u32, u32), items: Vec<(Option<u32>, Option<u32>)>) -> Vec<((i32, i32), (u32, u32))> {
         let (widths, heights): (Vec<Option<u32>>, Vec<Option<u32>>) = items.iter().cloned().unzip();
 
         let row_height = Self::get_size(&self.2, heights).unwrap_or(max_size.1);
@@ -196,7 +170,7 @@ impl Layout for Row {
         }).collect()
     }
 
-    fn size(&self, ctx: &mut Context, items: Vec<(Option<u32>, Option<u32>)>) -> (Option<u32>, Option<u32>) {
+    fn size(&self, _ctx: &mut Context, items: Vec<(Option<u32>, Option<u32>)>) -> (Option<u32>, Option<u32>) {
         let (widths, heights): (Vec<_>, Vec<_>) = items.into_iter().unzip();
         let items = widths.len();
         (
@@ -207,15 +181,15 @@ impl Layout for Row {
 }
 
 #[derive(Clone, Debug, Component)]
-pub struct Test(Row, Text, Text);
+pub struct Test(#[layout] Row, Text, Text, #[skip] #[ignore] u32, #[skip] i32);
 
 impl Test {
     pub fn new(font: Font, _font2: Font) -> Self {
         Test(
-            //Stack((Offset::Start, Offset::Center), (Size::Fit, Size::Fill)),
             Row(10, Offset::Center, Size::Fit),
             Text::new("HELLO HE", Color::from_hex("eb343a", 255), None, 48, 60, font.clone()),
             Text::new("HELLO HL", Color::from_hex("eb343a", 255), None, 24, 30, font.clone()),
+            30, -30
         )
     }
 }
@@ -229,121 +203,28 @@ impl Component for Test {
         println!("size: {:?}", self.size(ctx));
     }
 
-    fn on_click(&mut self, ctx: &mut Context, position: Option<(u32, u32)>) -> bool {
+    fn on_click(&mut self, _ctx: &mut Context, position: Option<(u32, u32)>) -> bool {
         println!("position: {:?}", position);
         true
     }
-    fn on_move(&mut self, ctx: &mut Context, position: Option<(u32, u32)>) -> bool {
+    fn on_move(&mut self, _ctx: &mut Context, _position: Option<(u32, u32)>) -> bool {
         true
     }
 }
 
 
-    //container![&mut self.1, &mut self.2]
-  //fn build(&mut self, ctx: &mut Context, max_size: (u32, u32)) -> Vec<((i32, i32), (u32, u32), &mut BoxComponent)> {
-  //    let mut offset = 0;
-  //    self.1.iter_mut().flat_map(|c| {
-  //        if max_size.1 < offset {None} else {
-  //            let size = c.size(ctx, (max_size.0, max_size.1-offset));
-  //            let v = ((0, offset as i32), size, c);
-  //            offset += size.1+self.0;
-  //            Some(v)
-  //        }
-  //    }).collect()
-  //}
-    //fn children_mut(&mut self) -> Vec<&mut BoxComponent> {self.1.iter_mut().collect()}
-
-  //fn on_click(&mut self, ctx: &mut Context, max_size: (u32, u32), position: (u32, u32)) {
-  //    let mut offset = 0;
-  //    for c in &mut self.1 {
-  //        let size = c.size(ctx, (max_size.0, max_size.1-offset));
-  //        if size.0 > position.0 && offset+size.1 > position.1 {
-  //            c.on_click(
-  //                ctx,
-  //                (max_size.0, offset+self.0),
-  //                (position.0, position.1-offset)
-  //            );
-  //            return;
-  //        }
-  //        if offset+size.1+self.0 > position.1 {
-  //            return;
-  //        }
-  //        offset += self.0 + size.1;
-  //    }
-  //}
-
-//  #[derive(Clone)]
-//  pub struct MyCom{
-//      font: resources::Font,
-//      text: String
-//  }
-
-//  impl Component for MyCom {
-//      fn build(&self, ctx: &mut Context, max_size: (u32, u32)) -> Vec<(Offset, Size, BoxComponent)> {
-//          vec![
-//              (Offset::Center, Size::Static(900, 200), vec![Box::new(
-//                  Shape(ShapeType::Rectangle(0), Color(0, 0, 255, 255)),
-//                  Image(ShapeType::Rectangle(0), image, Some(Color(255, 0, 0, 128))
-//              ]) as BoxComponent),
-
-
-
-
-//              (Offset::Center, Size::default(), Box::new(
-//                  Text(self.text.clone(), Color::from_hex("eb343a", 255), 48, 60, self.font.clone())
-//              )),
-//              (Offset::BottomRight, Size::default(), Box::new(
-//                  Text(self.text.clone(), Color::from_hex("eb343a", 255), 48, 60, self.font.clone())
-//              ))
-//          ]
-
-//        //container!(Offset::BottomRight, Size::Expand, [
-//        //    Shape(ShapeType::Rectangle(0), Color(0, 0, 255, 255)).size(Size::Static(1000, 200))
-//        //    text
-//        //])
-
-//        //vec![Box::new(Container(Offset::BottomCenter, Size::Static(500, 200),
-//        //    vec![
-//        //        Box::new(Shape(ShapeType::Rectangle(0), Color(0, 0, 255, 255))) as Box<dyn Component>,
-//        //        Box::new(text) as Box<dyn Component>,
-//        //    ]
-//        //))]
-//      }
-
-//      fn on_click(&mut self, ctx: &mut Context, max_size: (u32, u32), position: (u32, u32)) {
-//          self.text = format!("{:?}", position);
-//      }
-//  }
-
 pub struct MyApp;
 
 impl App for MyApp {
     async fn new(ctx: &mut Context<'_>) -> BoxComponent {
-        //PelicanContext.init(ctx);
         println!("START APP");
 
         let font = resources::Font::new(ctx, include_bytes!("../assets/fonts/outfit_bold.ttf").to_vec());
         let font2 = resources::Font::new(ctx, include_bytes!("../assets/fonts/outfit_regular.ttf").to_vec());
         let test = Test::new(font, font2);
+        test.answer();
+        todo!();
         Box::new(test)
-      //let image = resources::Image::new(ctx, image::load_from_memory(include_bytes!("../assets/icons/pfp3.png")).unwrap().into());
-      //let svg = resources::Image::svg(ctx, include_bytes!("../assets/icons/qr_code.svg"), 8.0);
-
-
-
-      //Box::new(Container(Column(50), vec![
-      //    //Some(Text("HELLO WORLD HEL", Color::from_hex("eb343a", 255), 48, 60, font.clone())).into(),
-      //    //Box::new(None::<Text>)
-      ////Box::new(Column(0, vec![
-
-      //    //Box::new(MyCom{font: font.clone(), text: "HEL".to_string()})
-      //    Box::new(Text::new("HELLO WORLD HEL", Color::from_hex("eb343a", 255), 48, 60, font.clone())),
-      //    Box::new(Text::new("HELLO HE", Color::from_hex("eb343a", 255), 48, 60, font.clone())),
-      //    Box::new(Text::new("HELLO HL", Color::from_hex("eb343a", 255), 24, 30, font.clone())),
-      //    Box::new(Text::new("HELLO EL", Color::from_hex("eb343a", 255), 48, 60, font.clone())),
-      //]))
-      //]))
-        //Box::new(Icon::new(ctx, IconName::Door, "ffffff", 48))
     }
 }
 
