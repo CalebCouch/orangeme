2fa5956cd191050a1d49c496dd7062277fa977ba
CalebCouch <calebblackcat@gmail.com>
2025-03-31 11:49:34 -0400

    test

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,42 @@ use pelican_ui::prelude::*;
 use rust_on_rails::prelude::*;
 // use rust_on_rails::prelude::Text as BasicText;
 
+#[derive(Clone, Debug, Component)]
+pub struct Bumper(Row, Button, Button);
+impl Events for Bumper {}
+
+impl Bumper {
+    pub fn new(ctx: &mut Context) -> Self {
+        Bumper(
+            Row(20, Offset::Center, Size::Fit),
+            Button::new(
+                ctx,
+                Some(AvatarContent::Icon("profile", CircleIconStyle::Secondary)),
+                None,
+                Some("Paste"),
+                None,
+                ButtonSize::Large,
+                ButtonWidth::Expand,
+                ButtonStyle::Primary,
+                ButtonState::Default,
+                |_ctx: &mut Context, position: (u32, u32)| println!("BOTTETTTEN...: {:?}", position)
+            ),
+            Button::new(
+                ctx,
+                Some(AvatarContent::Icon("profile", CircleIconStyle::Secondary)),
+                None,
+                Some("Paste"),
+                None,
+                ButtonSize::Large,
+                ButtonWidth::Expand,
+                ButtonStyle::Secondary,
+                ButtonState::Default,
+                |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position)
+            )
+        )
+    }
+}
+
 pub struct MyApp;
 
 impl App for MyApp {
@@ -19,18 +55,19 @@ impl App for MyApp {
         // Box::new(CircleIconData::new(ctx, "wallet", CircleIconStyle::Brand, 128))
         //Box::new(Avatar::new(ctx, AvatarContent::Icon("profile", CircleIconStyle::Secondary), Some(("microphone", CircleIconStyle::Danger)), false, 128))
         //Box::new(Button::secondary(ctx, Some("paste"), "Paste", None, ))
-        Box::new(Button::new(
-            ctx,
-            Some(AvatarContent::Icon("profile", CircleIconStyle::Secondary)),
-            None,
-            Some("Paste"),
-            None,
-            ButtonSize::Large,
-            ButtonWidth::Hug,
-            ButtonStyle::Secondary,
-            ButtonState::Default,
-            |_ctx: &mut Context, _position: (u32, u32)| println!("Pasting...")
-        ))
+        Box::new(Bumper::new(ctx))
+      //Box::new(Button::new(
+      //    ctx,
+      //    Some(AvatarContent::Icon("profile", CircleIconStyle::Secondary)),
+      //    None,
+      //    Some("Paste"),
+      //    None,
+      //    ButtonSize::Large,
+      //    ButtonWidth::Expand,
+      //    ButtonStyle::Primary,
+      //    ButtonState::Default,
+      //    |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position)
+      //))
         // Box::new(Button::primary(ctx, "wallet", (|| println!("Pasting..."))))
         // Box::new(BasicText::new("Continue", color, 48, 60, font.clone()))
         // Box::new(Text::new(ctx, "Continue", TextStyle::Label(color), 48))
