1014afc3b1208ee16b8f7b2f47dc9e6338e26a40
Ella Mae <ella@orange.me>
2025-03-31 21:53:54 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -5,41 +5,57 @@ use pelican_ui::prelude::*;
 use rust_on_rails::prelude::*;
 // use rust_on_rails::prelude::Text as BasicText;
 
-#[derive(Clone, Debug, Component)]
-pub struct Bumper(Row, Button, Button);
-impl Events for Bumper {}
+// #[derive(Clone, Debug, Component)]
+// pub struct Bumper(Row, Button, Button);
+// impl Events for Bumper {}
 
-impl Bumper {
-    pub fn new(ctx: &mut Context) -> Self {
-        Bumper(
-            Row(16, Offset::Center, Size::Fit),
-            Button::new(
-                ctx,
-                Some(AvatarContent::Icon("profile", AvatarIconStyle::Secondary)),
-                None,
-                Some("Paste"),
-                None,
-                ButtonSize::Large,
-                ButtonWidth::Expand,
-                ButtonStyle::Primary,
-                ButtonState::Default,
-                |_ctx: &mut Context, position: (u32, u32)| println!("BOTTETTTEN...: {:?}", position)
-            ),
-            Button::new(
-                ctx,
-                Some(AvatarContent::Icon("settings", AvatarIconStyle::Secondary)),
-                None,
-                Some("Paste"),
-                None,
-                ButtonSize::Large,
-                ButtonWidth::Expand,
-                ButtonStyle::Secondary,
-                ButtonState::Default,
-                |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position)
-            )
-        )
-    }
-}
+// impl Bumper {
+//     pub fn new(ctx: &mut Context) -> Self {
+//         Bumper(
+//             Row(16, Offset::Center, Size::Fit),
+//             Button::primary(ctx, "Receive", |_ctx: &mut Context, position: (u32, u32)| println!("Receive...: {:?}", position)),
+//             Button::primary(ctx, "Send", |_ctx: &mut Context, position: (u32, u32)| println!("Send...: {:?}", position))
+//             // Button::new(
+//             //     ctx,
+//             //     None,
+//             //     None,
+//             //     Some("Receive ❤️"),
+//             //     None,
+//             //     ButtonSize::Large,
+//             //     ButtonWidth::Expand,
+//             //     ButtonStyle::Secondary,
+//             //     ButtonState::Default,
+//             //     Offset::Center,
+//             //     |_ctx: &mut Context, position: (u32, u32)| println!("BOTTETTTEN...: {:?}", position)
+//             // ),
+//             // Button::new(
+//             //     ctx,
+//             //     None,
+//             //     None,
+//             //     Some("Send"),
+//             //     None,
+//             //     ButtonSize::Large,
+//             //     ButtonWidth::Expand,
+//             //     ButtonStyle::Primary,
+//             //     ButtonState::Default,
+//             //     Offset::Center,
+//             //     |_ctx: &mut Context, position: (u32, u32)| println!("BOTTETTTEN...: {:?}", position)
+//             // ),
+//             // Button::new(
+//             //     ctx,
+//             //     None, //Some(AvatarContent::Icon("profile", AvatarIconStyle::Secondary)),
+//             //     Some("send"),
+//             //     Some("Send"),
+//             //     None,
+//             //     ButtonSize::Medium,
+//             //     ButtonWidth::Hug,
+//             //     ButtonStyle::Secondary,
+//             //     ButtonState::Default,
+//             //     |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position)
+//             // )
+//         )
+//     }
+// }
 
 pub struct MyApp;
 
@@ -48,14 +64,16 @@ impl App for MyApp {
         let plugin = PelicanUI::init(ctx);
         ctx.configure_plugin(plugin);
 
-      //let color = ctx.get::<PelicanUI>().theme.colors.brand.secondary;
-      //Box::new(Icon::new(ctx, "error", color, 128))
+        let theme = &ctx.get::<PelicanUI>().theme;
+        //Box::new(Icon::new(ctx, "error", color, 128))
         //Box::new(Shape(ShapeType::Ellipse(20, (50, 50)), Color(155, 255, 0, 255)))
-        // Box::new(CircleIcon::new(ctx, ProfileImage::Icon("wallet", AvatarIconStyle::Brand), Some(("edit", AvatarIconStyle::Secondary)), false, 128))
-        // Box::new(CircleIconData::new(ctx, "wallet", AvatarIconStyle::Brand, 128))
-        //Box::new(Avatar::new(ctx, AvatarContent::Icon("profile", AvatarIconStyle::Secondary), Some(("microphone", AvatarIconStyle::Danger)), false, 128))
+        //Box::new(CircleIcon::new(ctx, ProfileImage::Icon("wallet", AvatarIconStyle::Brand), Some(("edit", AvatarIconStyle::Secondary)), false, 128))
+        //Box::new(CircleIconData::new(ctx, "wallet", AvatarIconStyle::Brand, 128))
+        Box::new(Avatar::new(ctx, AvatarContent::Icon("profile", AvatarIconStyle::Secondary), Some(("microphone", AvatarIconStyle::Danger)), false, 128))
         //Box::new(Button::secondary(ctx, Some("paste"), "Paste", None, ))
-        // Box::new(Bumper::new(ctx))
+        // let bumper = Bumper::new(ctx);
+        // let bumper = Image(ShapeType::Rectangle(0, (300, 300)), theme.brand.logo.wordmark.clone(), Some(Color::from_hex("ffffff", 255)));
+        // Box::new(bumper)
         // Box::new(IconButton::new(
         //     ctx,
         //     Some("close"),
@@ -80,19 +98,21 @@ impl App for MyApp {
         // Box::new(BasicText::new("Continue", color, 48, 60, font.clone()))
         // Box::new(Text::new(ctx, "Continue", TextStyle::Label(color), 48))
 
-        Box::new(
-            TextInput::new(
-                ctx,
-                None,
-                None,
-                "Search names...",
-                Some(("close", |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position))),
-                None,
-                None
-            )
-        )
+        // Box::new(
+        //     TextInput::new(
+        //         ctx,
+        //         None,
+        //         None,
+        //         "Search names...",
+        //         Some(("close", |_ctx: &mut Context, position: (u32, u32)| println!("Pasting...: {:?}", position))),
+        //         None,
+        //         None
+        //     )
+        // )
 
         // Box::new(Alert::new(ctx, "You were booted from this room."))
+
+        // Box::new(QRCode::new(ctx, "heaxig84aks4d39EID28EXOCrad49oaniIoahriCwea24"))
     }
 }
 
