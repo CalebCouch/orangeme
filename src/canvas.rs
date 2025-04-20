bf22cb44d510a6916c001a84242630ad3d4c8aa2
CalebCouch <calebblackcat@gmail.com>
2025-04-20 12:47:52 -0400

    test

---
+++ src/canvas.rs
@@ -0,0 +1,87 @@
+use rust_on_rails::prelude::*;
+use serde::{Serialize, Deserialize};
+use std::sync::mpsc::{channel, Sender, Receiver};
+use std::time::Duration;
+
+#[derive(Default)]
+pub struct MyBackgroundTask;
+#[async_trait]
+impl Task for MyBackgroundTask {
+    fn interval(&self) -> Option<Duration> {Some(Duration::from_secs(1))}
+    async fn run(&mut self, ctx: &mut HeadlessContext) {
+        println!("background tick");
+        let Modify(modify) = ctx.cache.get::<Modify>().await;
+        let CacheCount(cc) = ctx.cache.get::<CacheCount>().await;
+        ctx.cache.set(CacheCount(cc+modify)).await;
+    }
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct Count(i32);
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct Modify(i32);
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct CacheCount(i32);
+
+pub struct Channel<I, O>(Receiver<I>, Sender<O>);
+
+impl<I, O> Channel<I, O> {
+    pub fn new() -> (Self, Channel<O, I>) {
+        let (si, ri) = channel::<I>();
+        let (so, ro) = channel::<O>();
+        (Channel(ri, so), Channel(ro, si))
+    }
+}
+
+pub struct MyTask(Channel<i32, i32>);
+
+#[async_trait]
+impl Task for MyTask {
+    fn interval(&self) -> Option<Duration> {Some(Duration::from_secs(1))}
+
+    async fn run(&mut self, ctx: &mut HeadlessContext) {
+        println!("running task");
+        let Modify(modify) = ctx.cache.get::<Modify>().await;
+        let modify = modify + self.0.0.try_iter().sum::<i32>();
+        ctx.cache.set(Modify(modify)).await;
+        let CacheCount(cc) = ctx.cache.get::<CacheCount>().await;
+        self.0.1.send(cc).unwrap();
+    }
+}
+
+pub struct MyApp {
+    channel: Channel<i32, i32>,
+    font: Font
+}
+
+impl App for MyApp {
+    async fn background_tasks(_ctx: &mut HeadlessContext) -> Tasks {
+        tasks![MyBackgroundTask]
+    }
+    async fn new(ctx: &mut Context<'_>) -> (Self, Tasks) {
+        let font = ctx.add_font(include_bytes!("../resources/fonts/outfit_regular.ttf"));
+        let (channel, input) = Channel::<i32, i32>::new();
+        (MyApp{channel, font}, tasks![MyTask(input)])
+    }
+
+    fn on_event(&mut self, ctx: &mut Context<'_>, event: Event) {
+        match event {
+            Event::Tick => {
+                if let Some(latest) = self.channel.0.try_iter().last() {
+                    ctx.state().set(&Count(latest));
+                }
+                let text = format!("Count: {}", ctx.state().get::<Count>().0);
+                ctx.clear(Color(0, 0, 255, 255));
+                ctx.draw(Area((200.0, 200.0), None), CanvasItem::Shape(Shape::Rectangle(20.0, (ctx.size().0/2.0, ctx.size().1/2.0)), Color(255, 255, 0, 255)));
+                ctx.draw(Area((250.0, 250.0), None), CanvasItem::Text(Text::new(&text, Color(255, 0, 0, 255), self.font.clone(), 48.0, 60.0, None)));
+            },
+            Event::Mouse{..} => self.channel.1.send(-1).unwrap(),
+            Event::Keyboard{..} => self.channel.1.send(1).unwrap(),
+            _ => {}
+        }
+    }
+}
+
+create_entry_points!(MyApp);
--- src/lib.rs
+++ src/lib.rs
@@ -1,107 +1,178 @@
-
-use pelican_ui::prelude::*;
 use rust_on_rails::prelude::*;
-
+use rust_on_rails::prelude::Text as BasicText;
+use pelican_ui::prelude::*;
 use serde::{Serialize, Deserialize};
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::time::Duration;
 
-#[derive(Serialize, Deserialize, Default, Debug)]
-pub struct Count(u64);
+//FOR TESTING DO NOT REMOVE
+//mod canvas;
+//pub use canvas::*;
 
-#[derive(Serialize, Deserialize, Default, Debug)]
-pub struct CacheCount(u64);
+//  mod flows;
+//  use crate::flows::*;
 
-pub struct MyBackgroundApp{}
+//  use std::time::Duration;
 
-impl MyBackgroundApp {
-    async fn tick5(&mut self, ctx: &mut AsyncContext) {
-        let count = ctx.cache.get::<CacheCount>().await.0;
-        println!("on_background+tick: {}", count+1);
-        ctx.cache.set(CacheCount(count+1)).await;
+//  #[derive(Serialize, Deserialize, Default, Debug)]
+//  pub struct Count(u64);
+
+//  #[derive(Serialize, Deserialize, Default, Debug)]
+//  pub struct CacheCount(u64);
+
+//  //  pub struct MyBackgroundApp{
+//  //      cache: Cache
+//  //  }
+
+//  //  impl MyBackgroundApp {
+//  //      async fn tick5(&mut self) {
+//  //          let count = self.cache.get::<CacheCount>().await.0;
+//  //          println!("on_background+tick: {}", count+1);
+//  //          self.cache.set(CacheCount(count+1)).await;
+//  //      }
+//  //  }
+
+//  //  impl HeadlessApp for MyBackgroundApp {
+//  //      const LOG_LEVEL: log::Level = log::Level::Error;
+//  //      async fn new(cache: Cache) -> Self {
+//  //          MyBackgroundApp{cache}
+//  //      }
+
+//  //      async fn register_tasks() -> BackgroundTasks {
+//  //          vec![
+//  //              task!(Duration::from_secs(5), MyBackgroundApp::tick5)
+//  //          ]
+//  //      }
+//  //  }
+
+//  //  pub struct ActiveApp{
+//  //      cache: Cache,
+//  //      results: Sender<Callback>
+//  //  }
+
+//  //  impl ActiveApp {
+//  //      pub fn new(cache: cache, results: Sender<Callback>) -> Self {
+//  //          ActiveApp{cache, results}
+//  //      }
+
+//  //      async fn async_tick(&mut self) {
+//  //          let cache_count = self.cache.get::<CacheCount>().await.0;
+//  //          println!("Async Tick 1 secs apart, cache_count: {}", cache_count);
+//  //          self.results.send(Box::new(move |state: &mut State| {
+//  //              let count = state.get::<Count>().0;
+//  //              state.set(&CacheCount(cache_count));
+//  //              state.set(&Count(count + 1));
+//  //          })).unwrap();
+//  //      }
+//  //  }
+
+//  //  impl HeadlessApp for ActiveApp {
+//  //      fn register_tasks() -> Vec<Task<Self>> {
+//  //          vec![
+//  //              task!(Duration::from_secs(1), MyApp::async_tick)
+//  //          ]
+//  //      }
+//  //  }
+
+
+//  use bdk_wallet::{Wallet, KeychainKind, ChangeSet};
+//  use bdk_wallet::descriptor::template::Bip86;
+//  use bdk_wallet::bitcoin::bip32::Xpriv;
+//  use bdk_wallet::bitcoin::Network;
+//  use bdk_wallet::{PersistedWallet, WalletPersister};
+//  use bdk_wallet::chain::Merge;
+
+//  #[derive(Serialize, Deserialize, Default, Clone, Debug)]
+//  pub struct WalletKey(Option<Xpriv>);
+
+//  #[derive(Serialize, Deserialize, Default, Debug)]
+//  pub struct WalletInfo(ChangeSet);
+
+//  #[derive(Serialize, Deserialize, Default, Debug)]
+//  pub struct MemoryPersister(ChangeSet);
+//  impl WalletPersister for MemoryPersister {
+//      type Error = ();
+//      fn initialize(persister: &mut Self) -> Result<ChangeSet, Self::Error> {Ok(persister.0.clone())}
+//      fn persist(persister: &mut Self, changeset: &ChangeSet) -> Result<(), Self::Error> {Ok(persister.0.merge(changeset.clone()))}
+//  }
+
+//  pub struct BDKPlugin {
+//      wallet: PersistedWallet<MemoryPersister>
+//  }
+
+//  impl BDKPlugin {
+//      pub async fn init(cache: &mut Cache) -> Self {
+//          let mut db = cache.get::<MemoryPersister>().await; 
+//          let (ext, int) = Self::get_descriptors(cache).await;
+//          let network = Network::Bitcoin;
+//          let wallet_opt = Wallet::load()
+//              .descriptor(KeychainKind::External, Some(ext.clone()))
+//              .descriptor(KeychainKind::Internal, Some(int.clone()))
+//              .extract_keys()
+//              .check_network(network)
+//              .load_wallet(&mut db)
+//              .expect("wallet");
+//          let mut wallet = match wallet_opt {
+//              Some(wallet) => wallet,
+//              None => Wallet::create(ext, int)
+//                  .network(network)
+//                  .create_wallet(&mut db)
+//                  .expect("wallet"),
+//          };
+
+//          BDKPlugin{wallet}
+//      }
+
+//      pub async fn get_descriptors(cache: &mut Cache) -> (Bip86<Xpriv>, Bip86<Xpriv>) {
+//          //TODO: from web 5 normally
+//          let key = cache.get::<WalletKey>().await.0.unwrap_or(
+//              Xpriv::new_master(Network::Bitcoin, &secp256k1::SecretKey::new(&mut secp256k1::rand::thread_rng()).secret_bytes()).unwrap()
+//          );
+//          cache.set(WalletKey(Some(key.clone()))).await;
+//          (Bip86(key.clone(), KeychainKind::External), 
+//           Bip86(key, KeychainKind::Internal))
+//      }
+//  }
+//
+pub struct BDKPlugin;
+impl BDKPlugin {
+    async fn init(&mut self) {//Include theme
+        println!("Initialized BDK");
     }
 }
 
-impl BackgroundApp for MyBackgroundApp {
-    const LOG_LEVEL: log::Level = log::Level::Error;
-    async fn new(ctx: &mut AsyncContext) -> Self {
-        MyBackgroundApp{}
-    }
+impl Plugin for BDKPlugin {
+    async fn background_tasks(_ctx: &mut HeadlessContext) -> Tasks {vec![]}
 
-    async fn register_tasks(&mut self, ctx: &mut AsyncContext) -> BackgroundTasks {
-        vec![
-            background_task!(Duration::from_secs(5), MyBackgroundApp::tick5)
-        ]
+    async fn new(ctx: &mut Context<'_>, h_ctx: &mut HeadlessContext) -> (Self, Tasks) {
+        (BDKPlugin{}, vec![])
     }
 }
 
-#[cfg(target_os = "ios")]
-extern "C" {
-    fn get_clipboard_string() -> *const std::os::raw::c_char;
-}
-
-pub fn clipboard() -> String {
-    #[cfg(target_os = "ios")]
-    unsafe {
-        let ptr = get_clipboard_string();
-        if ptr.is_null() {
-            return String::new();
-        }
-
-        let cstr = std::ffi::CStr::from_ptr(ptr);
-        let string = cstr.to_string_lossy().into_owned();
-
-        return string
-    }
-
-    String::new()
-}
-
-
-mod flows;
-use crate::flows::*;
 
 pub struct MyApp;
 
-impl MyApp {
-    async fn async_tick(ctx: &mut AsyncContext) -> Callback {
-        let cache_count = ctx.cache.get::<CacheCount>().await.0;
-        println!("Async Tick 1 secs apart, cache_count: {}", cache_count);
-        Box::new(move |state: &mut State| {
-            let count = state.get::<Count>().0;
-            state.set(&CacheCount(cache_count));
-            state.set(&Count(count + 1));
-        })
-    }
-}
-
 impl App for MyApp {
-    async fn register_tasks() -> AsyncTasks {
-        vec![
-            async_task!(Duration::from_secs(1), MyApp::async_tick)
-        ]
+    //TODO: include_plugins![BDKPlugin]; || #[derive(Plugins[BDKPlugin])] || #[Plugins[BDKPlugin]]
+    async fn background_tasks(ctx: &mut HeadlessContext) -> Tasks {
+        BDKPlugin::background_tasks(ctx).await
     }
 
-    async fn root(ctx: &mut Context<'_, '_>) -> Box<dyn Drawable> {
-        let plugin = PelicanUI::init(ctx);
-        ctx.configure_plugin(plugin);
+    async fn plugins(ctx: &mut Context<'_>, h_ctx: &mut HeadlessContext) -> (Plugins, Tasks) {
+        let (plugin, tasks) = BDKPlugin::new(ctx, h_ctx).await;
+       
+        (std::collections::HashMap::from([(std::any::TypeId::of::<BDKPlugin>(), Box::new(plugin) as Box<dyn std::any::Any>)]), tasks)
+    }
 
-        // let theme = &ctx.get::<PelicanUI>().theme;      
-        
-        // scan_qr();
-        
-
-        // let flow = OrangeFlow::Bitcoin(BitcoinFlow::Home(page));
-        // let text = pelican_ui::prelude::Text::new(ctx, "Test Text", TextStyle::Primary, 48);
-        // let size = Size::max()
-        // Box::new(text)
-
-        // let flow = BitcoinFlow::Home(BitcoinHome::new(ctx));
+    async fn new(ctx: &mut Context<'_>) -> Box<dyn Drawable> {
+        ctx.get::<BDKPlugin>().init();
         let page = ScanQR::new(ctx).page();
+        let font = ctx.add_font(include_bytes!("../resources/fonts/outfit_regular.ttf"));
+
+        
         Box::new(Interface::new(ctx, page))
-
-        // Box::new(Button::secondary(ctx, Some("edit"), "Edit Address", None, |_ctx: &mut Context| println!("Button")))
-
+        //Box::new(BasicText::new("Hello", Color(0, 0, 255, 255), None, 48.0, 60.0, font))
     }
 }
 
-create_entry_points!(MyApp, MyBackgroundApp);
+create_entry_points!(MyApp);
