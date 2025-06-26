2b4f0518c73ad673a520d3acd5f0d0e0cc402ab2
Ella Mae <ella@orange.me>
2025-06-26 09:47:26 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use profiles::plugin::{ProfilePlugin, NameGenerator};
 use profiles::service::{Name, Profiles, ProfileService};
 use profiles::components::AvatarContentProfiles;
 use messages::plugin::MessagesPlugin;
+use std::sync::{Arc, Mutex};
 use std::any::TypeId;
 use std::collections::BTreeMap;
 use std::pin::Pin;
@@ -15,8 +16,11 @@ use bitcoin::service::BDKService;
 use messages::service::RoomsService;
 
 use bitcoin::pages::*;
+use bitcoin::components::IconButtonBitcoin;
 use messages::pages::*;
+use messages::components::IconButtonMessages;
 use profiles::pages::*;
+use profiles::components::IconButtonProfiles;
 
 use pelican_ui_std::{Stack, Splash};
 use pelican_ui::layout::{Area, SizeRequest, Layout};
@@ -52,45 +56,39 @@ impl Application for MyApp {
 start!(MyApp);
 
 #[derive(Debug, Component)]
-pub struct App(Stack, Interface, #[skip] bool);
+pub struct App(Stack, Interface);
 
 impl App {
     pub fn new(ctx: &mut Context) -> Box<Self> {
-        let start = Splash::new(ctx);
-        Box::new(App(Stack::default(), Interface::new(ctx, Box::new(start), None), false))
+        let mut account_actions = Arc::new(Mutex::new(vec![
+            IconButtonBitcoin::new(ctx), // AccountAction("wallet", Box::new(|ctx: &mut Context| Box::new(BitcoinHome::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>),
+            IconButtonMessages::new(ctx),
+            IconButtonProfiles::block(ctx) // AccountAction("block", Box::new(|ctx: &mut Context| Box::new(BitcoinHome::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>)
+        ]));
+
+        let navigation = vec![
+            ("wallet", "Bitcoin".to_string(), None, Some(Box::new(|ctx: &mut Context| Box::new(BitcoinHome::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>)),
+            ("messages", "Messages".to_string(), None, Some(Box::new(move |ctx: &mut Context| Box::new(MessagesHome::new(ctx, account_actions.clone())) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>)),
+            ("profile", "My Profile".to_string(), Some(AvatarContentProfiles::default()), Some(Box::new(|ctx: &mut Context| Box::new(Account::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>))
+        ];
+
+        let home = BitcoinHome::new(ctx);
+        let interface = Interface::new(ctx, Box::new(home), Some((0_usize, navigation)));
+        Box::new(App(Stack::default(), interface))
     }
 }
 
 impl OnEvent for App {
     fn on_event(&mut self, ctx: &mut Context, event: &mut dyn Event) -> bool {
         if let Some(TickEvent) = event.downcast_ref::<TickEvent>() {
-            if !self.2 {
-                if let Some(me) = ctx.state().get::<Name>() {
-                    let me = me.0.clone();
-                    let avatar = AvatarContentProfiles::from_orange_name(ctx, &me);
-                    let username = ProfilePlugin::get_username(ctx);
-                    let username = NameGenerator::display_name(username);
-
-                    let navigation = vec![
-                        ("wallet", "Bitcoin".to_string(), None, Some(Box::new(|ctx: &mut Context| Box::new(BitcoinHome::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>)),
-                        ("messages", "Messages".to_string(), None, Some(Box::new(|ctx: &mut Context| Box::new(MessagesHome::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>)),
-                        ("profile", username, Some(avatar), Some(Box::new(|ctx: &mut Context| Box::new(Account::new(ctx)) as Box<dyn AppPage>) as Box<dyn FnMut(&mut Context) -> Box<dyn AppPage>>))
-                    ];
-
-                    let home = BitcoinHome::new(ctx);
-                    self.1 = Interface::new(ctx, Box::new(home), Some((0_usize, navigation)));
-                    self.2 = true;
-                }
-            }
-
-            if self.2 {
+            if ctx.state().get::<Name>().is_some() {
                 self.1.desktop().as_mut().map(|d| d.navigator().as_mut().map(|nav| {
                     let me = ProfilePlugin::me(ctx).0;
                     nav.update_avatar(AvatarContentProfiles::from_orange_name(ctx, &me));
 
-                    let username = ProfilePlugin::get_username(ctx);
-                    let username = NameGenerator::display_name(username);
-                    nav.update_username(username)
+                    // let username = ProfilePlugin::get_username(ctx);
+                    // let username = NameGenerator::display_name(username);
+                    // nav.update_username(username)
                 }));
             }
         }
